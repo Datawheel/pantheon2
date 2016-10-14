c424b9919328cc2b8e1ab9b5eae1ad02e4ad959e
Melissa Teng <tengmelis@gmail.com>
2016-10-14 12:39:48 -0400

    fixed some style scale issues

--- app/components/profile/person/Header.jsx
+++ app/components/profile/person/Header.jsx
@@ -19,7 +19,7 @@ const Header = ({ pageviews, person }) => {
         <p className='top-desc'>Cultural Memory of</p>
         <h2 className='profile-type'>{person.profession.name}</h2>
         <h1 className='profile-name'>{person.name}</h1>
-        <p className='date-subtitle'>{person.birthyear.name} - {person.deathyear ? `— ${person.deathyear.name}` : "Present"}</p>
+        <p className='date-subtitle'>{person.birthyear.name} - {person.deathyear ? `${person.deathyear.name}` : "Present"}</p>
         <pre>
           <img className='sparkline' src={`/sparkline.svg`} alt={`Sparkline here`} />
         </pre>
