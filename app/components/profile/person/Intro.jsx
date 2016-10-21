162b7391e497506fb302d491651b115dd930f611
Dave Landry <landry.dave@gmail.com>
2016-10-21 16:05:09 -0400

    optimizes code in person Intro.jsx

--- app/components/profile/person/Intro.jsx
+++ app/components/profile/person/Intro.jsx
@@ -5,36 +5,23 @@ import iconProfW from 'images/globalNav/profile-w.svg';
 import {COLORS_DOMAIN} from "types";
 
 const Intro = ({ person }) => {
-  const today = new Date();
-  const year = today.getFullYear();
-  let age = year - person.birthyear.id;
-  if(person.deathyear) age = person.deathyear.id - person.birthyear.id;
 
-  const backgroundColor = COLORS_DOMAIN[person.profession.domain_slug];
+  const age = person.deathyear
+            ? person.deathyear.id - person.birthyear.id
+            : new Date().getFullYear() - person.birthyear.id,
+        backgroundColor = COLORS_DOMAIN[person.profession.domain_slug],
+        decoLines = 14;
 
   return (
-    <section className={'intro-section'}>
-      <div className={'intro-deco'}>
-        <div className={'deco-lines'}>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
-          <span className={'deco-line'} style={{backgroundColor}}></span>
+    <section className="intro-section">
+      <div className="intro-deco">
+        <div className="deco-lines">
+          { Array(decoLines).fill().map(i => <span key={i} className="deco-line" style={{backgroundColor}}></span>) }
         </div>
       </div>
-      <div className={'intro-content'}>
+      <div className="intro-content">
         <img src={`/people/${person.wiki_id}.jpg`} alt={`Photo of ${person.name}`} />
-        <div className={'intro-text'}>
+        <div className="intro-text">
           <h3>
             <img src={iconProfW} />
             {person.deathyear ?
@@ -48,7 +35,7 @@ const Intro = ({ person }) => {
               `${person.gender ? "She" : "He"} lived to be ${age} before passing in ${person.deathyear.name}.` : null }&nbsp;
             At {age} years old, {person.gender ? "she" : "he"} has reached (total page views) page views on Wikipedia, hitting a peak of (max page views) in (max page view year).
             By analyzing all "globally remembered people", Pantheon aims to understand cultural development through changes in professions, birth and death places, and Wikipedia activity.&nbsp;
-            <a href="/about/" className={'deep-link'}>Read more about our research</a>
+            <a href="/about/" className="deep-link">Read more about our research</a>
           </p>
         </div>
       </div>
