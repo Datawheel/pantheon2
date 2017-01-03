157f59c48ff14d492221610ba6f08fa51b75842d
Melissa Teng <tengmelis@gmail.com>
2017-01-03 17:36:50 -0500

    some of Cristian’s updates to About content

--- app/components/about/Contact.jsx
+++ app/components/about/Contact.jsx
@@ -4,8 +4,10 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>pantheon[at]media[dot]mit[dot]edu</p>
-      <p>75 Amherst Street, Cambridge, MA 02142</p>
+      <p>Pantheon team:</p>
+      <p>pantheon@media.mit.edu</p>
+      <p>Datawheel:</p>
+      <p>info@datawheel.us</p>
     </div>
   );
 };
