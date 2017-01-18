fcc43258634a814768ed574f43337fd0dd3c1fdd
Alexander Simoes <alexandersimoes@gmail.com>
2017-01-17 23:49:52 -0500

    no longer need place_occupation table

--- app/components/profile/place/Occupations.jsx
+++ app/components/profile/place/Occupations.jsx
@@ -1,14 +1,35 @@
-import React, { Component, PropTypes } from "react";
+import React from "react";
+import {nest} from "d3-collection";
 import AnchorList from "components/utils/AnchorList";
 
-const Occupations = ({place, occupationsBorn, occupationsDied}) => {
+const Occupations = ({place, peopleBorn, peopleDied}) => {
+  const occupationsBorn = nest()
+    .key(d => d.occupation.id)
+    .rollup(leaves => {
+      return {num_born: leaves.length, occupation: leaves[0].occupation}
+    })
+    .entries(peopleBorn.filter(d => d.occupation_id))
+    .sort((a, b) => b.value.num_born - a.value.num_born)
+    .map(d => d.value);
+  const occupationsDied = nest()
+    .key(d => d.occupation.id)
+    .rollup(leaves => {
+      return {num_died: leaves.length, occupation: leaves[0].occupation}
+    })
+    .entries(peopleDied.filter(d => d.occupation_id))
+    .sort((a, b) => b.value.num_born - a.value.num_born)
+    .map(d => d.value);
+
   return (
     <div>
       <p>
-        Most individuals born in {place.name} were <AnchorList items={occupationsBorn} name={(d) => `${d.occupation.occupation} (${d.num_born})`} url={(d) => `/profile/occupation/${d.occupation.occupation_slug}`} /> while most who died were <AnchorList items={occupationsDied} name={(d) => `${d.occupation.occupation} (${d.num_died})`} url={(d) => `/profile/occupation/${d.occupation.occupation_slug}`} />.
+        Most individuals born in {place.name} were&nbsp;
+        <AnchorList items={occupationsBorn.splice(0, 5)} name={(d) => `${d.occupation.occupation} (${d.num_born})`} url={(d) => `/profile/occupation/${d.occupation.occupation_slug}`} />,&nbsp;
+        while most who died were&nbsp;
+        <AnchorList items={occupationsDied.splice(0, 5)} name={(d) => `${d.occupation.occupation} (${d.num_died})`} url={(d) => `/profile/occupation/${d.occupation.occupation_slug}`} />.
       </p>
     </div>
   );
-}
+};
 
 export default Occupations;
