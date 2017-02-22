a2077f62a2e18dafc2b89345d77df6111958c42c
Alexander Simoes <alexandersimoes@gmail.com>
2017-02-22 16:18:29 -0500

    clean up place header closes #116

--- app/components/profile/place/Header.jsx
+++ app/components/profile/place/Header.jsx
@@ -1,35 +1,36 @@
-import React, { Component, PropTypes } from 'react';
-import styles from 'css/components/profile/header';
-import sparklineSvg from 'images/sparkline.svg';
-import placeholderImg from 'images/placeholder_place_hero.jpg';
-import { FORMATTERS } from "types";
+import React, {PropTypes} from "react";
+import styles from "css/components/profile/header";
+import sparklineSvg from "images/sparkline.svg";
+import placeholderImg from "images/placeholder_place_hero.jpg";
+import {FORMATTERS} from "types";
 
-const Header = ({ place, country }) => {
-  const placeImg = place.wiki_id && !place.img_link ? `/place/${country.id}.jpg` : `/place/${place.id}.jpg`;
+const Header = ({place, country}) => {
+  const placeImg = place.img_link ? `/place/${place.id}.jpg` : country.img_link ? `/place/${country.id}.jpg` : placeholderImg;
 
   return (
-    <header className='hero'>
-      <div className='bg-container'>
-        <div className='bg-img-mask place'>
-          <div className='bg-img bg-img-t' style={{backgroundImage: `url(${placeImg})`}}></div>
-          <div className='bg-img bg-img-b' style={{backgroundImage: `url(${placeImg})`}}></div>
+    <header className="hero">
+      <div className="bg-container">
+        <div className="bg-img-mask place">
+          <div className="bg-img bg-img-t" style={{backgroundImage: `url(${placeImg})`}}></div>
+          <div className="bg-img bg-img-b" style={{backgroundImage: `url(${placeImg})`}}></div>
         </div>
       </div>
-      <div className='info'>
-        <p className='top-desc'>Cultural Production in</p>
-        <h2 className='profile-type'>Present Day</h2>
-        <h1 className='profile-name'>{place.name}</h1>
-        { place.name !== place.country_name ? <p className='date-subtitle'>{place.country_name}</p> : null}
-        <p className='date-subtitle'>{FORMATTERS.year(country.soverign_date)} - Present</p>
+      <div className="info">
+        <p className="top-desc">Cultural Production in</p>
+        <h2 className="profile-type">Present Day</h2>
+        <h1 className="profile-name">{place.name}</h1>
+        { place.name !== place.country_name ? <p className="date-subtitle"><a href={`/profile/place/${country.slug}`}>{place.country_name}</a></p> : null}
+        <p className="date-subtitle">{FORMATTERS.year(country.soverign_date)} - Present</p>
         <pre>
-          <img className='sparkline' src={sparklineSvg} alt={`Sparkline here`} />
+          <img className="sparkline" src={sparklineSvg} alt="Sparkline here" />
         </pre>
       </div>
     </header>
   );
-}
+};
 
 Header.propTypes = {
+  country: PropTypes.object,
   place: PropTypes.object
 };
 
