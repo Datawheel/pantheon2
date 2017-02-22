1c90c4b0f51a1fe07621b0115b181f50ae8323b7
Melissa Teng <tengmelis@gmail.com>
2017-02-22 17:07:31 -0500

    adds share icons to profile sections
    for issue #109

--- app/components/profile/Section.jsx
+++ app/components/profile/Section.jsx
@@ -1,16 +1,24 @@
 import React, {Component, PropTypes} from "react";
 import styles from "css/components/profile/section";
+import downloadSvg from "images/icon-download.svg";
+import shareSvg from "images/icon-share.svg";
 
 const Section = ({children, index, numSections, slug, title}) => {
   return (
     <section className="profile-section" key={slug}>
       <div className="section-head">
-        <a name={slug}><h4>{title}</h4></a>
-        <span className="section-nav">
-          {[...Array(numSections)].map((x, i) =>
-            i === index ? <span key={i} className="icon-circle active"></span> : <span key={i} className="icon-circle"></span>
-          )}
-        </span>
+        <div className="section-title">
+          <a name={slug}><h4>{title}</h4></a>
+          <span className="section-nav">
+            {[...Array(numSections)].map((x, i) =>
+              i === index ? <span key={i} className="icon-circle active"></span> : <span key={i} className="icon-circle"></span>
+            )}
+          </span>
+        </div>
+        <div className="section-actions">
+          <a className="section-share"><img src={shareSvg} alt={`Share this visualization`} /></a>
+          <a className="section-download"><img src={downloadSvg} alt={`Download this visualization`} /></a>
+        </div>
       </div>
       <div className="section-body">
         { children }
