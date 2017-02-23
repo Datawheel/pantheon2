8b7f817c8c9ca87dc589c3549fa72a1ad8ec53e4
Dave Landry <landry.dave@gmail.com>
2017-02-23 18:19:31 -0500

    updates d3plus-react to v0.2

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,9 +1,10 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from "react";
 import {connect} from "react-redux";
-import { activateSearch } from 'actions/users';
-import Navigation from 'containers/Navigation';
-import Search from 'components/Search';
-import styles from 'css/main';
+import {activateSearch} from "actions/users";
+import Navigation from "containers/Navigation";
+import Search from "components/Search";
+import d3plus from "types/d3plus";
+import "css/main";
 
 class App extends Component {
 
@@ -11,10 +12,16 @@ class App extends Component {
     super(props);
   }
 
+  getChildContext() {
+    return {
+      d3plus: this.props.d3plus || {}
+    };
+  }
+
   render() {
-    const { children, searchActive, location } = this.props;
+    const {children, searchActive, location} = this.props;
     return (
-      <div className={ location.pathname === '/' ? 'app home' : 'app'}>
+      <div className={ location.pathname === "/" ? "app home" : "app"}>
         { searchActive ? <Search /> : null }
         <Navigation />
         {children}
@@ -22,17 +29,17 @@ class App extends Component {
     );
   }
 
-  componentDidMount(){
+  componentDidMount() {
     document.addEventListener("keydown", () => {
       // 's' key
-      if(event.keyCode == 83){
-        if(event.target.tagName != "INPUT"){
+      if (event.keyCode === 83) {
+        if (event.target.tagName !== "INPUT") {
           event.preventDefault();
           this.props.activateSearch();
         }
       }
       // 'esc' key
-      if(event.keyCode == 27){
+      if (event.keyCode === 27) {
         event.preventDefault();
         this.props.activateSearch();
       }
@@ -40,8 +47,15 @@ class App extends Component {
   }
 }
 
+App.childContextTypes = {
+  d3plus: React.PropTypes.object
+};
+
+App.defaultProps = {d3plus};
+
 App.propTypes = {
   children: PropTypes.object,
+  d3plus: React.PropTypes.object,
   searchActive: PropTypes.bool.isRequired
 };
 
@@ -51,4 +65,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { activateSearch })(App);
+export default connect(mapStateToProps, {activateSearch})(App);
