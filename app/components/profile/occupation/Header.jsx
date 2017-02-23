2e8e2e4f9fabdc60744932834322ae5414a47344
Melissa Teng <tengmelis@gmail.com>
2017-02-23 14:12:14 -0500

    changes color of Occ splash to match domain

--- app/components/profile/occupation/Header.jsx
+++ app/components/profile/occupation/Header.jsx
@@ -2,13 +2,14 @@ import React, {Component, PropTypes} from "react";
 import styles from "css/components/profile/header";
 import sparklineSvg from "images/sparkline.svg";
 import placeholderImg from "images/placeholder_prof_hero.jpg";
+import {COLORS_DOMAIN} from "types";
 
 const Header = ({ occupation, people }) => {
 
   return (
     <header className="hero">
       <div className="bg-container">
-        <div className="bg-img-mask profession">
+        <div style={{backgroundColor: COLORS_DOMAIN[occupation.domain_slug]}} className="bg-img-mask profession">
           <div className="bg-img bg-img-t">
             {people.slice(0, 4).map(p =>
               <img key={p.id} src={`/people/${p.id}.jpg`} />
@@ -24,7 +25,7 @@ const Header = ({ occupation, people }) => {
               <img key={p.id} src={`/people/${p.id}.jpg`} />
             )}
           </div>
-          <div className="bg-img-mask-after"></div>
+          <div style={{backgroundColor: COLORS_DOMAIN[occupation.domain_slug]}} className="bg-img-mask-after"></div>
         </div>
       </div>
       <div className="info">
