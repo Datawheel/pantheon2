c72ce00b48d63ee3361bd940a12a927b90149e50
Alexander Simoes <alexandersimoes@gmail.com>
2017-02-24 15:40:13 -0500

    occupation sparkline closes #72

--- app/components/profile/occupation/Header.jsx
+++ app/components/profile/occupation/Header.jsx
@@ -1,11 +1,16 @@
-import React, {Component, PropTypes} from "react";
-import styles from "css/components/profile/header";
+import React, {PropTypes} from "react";
+import "css/components/profile/header";
 import sparklineSvg from "images/sparkline.svg";
-import placeholderImg from "images/placeholder_prof_hero.jpg";
 import {COLORS_DOMAIN} from "types";
 import {plural} from "pluralize";
+import {LinePlot} from "d3plus-react";
 
-const Header = ({ occupation, people }) => {
+const Header = ({occupation, people}) => {
+
+  const dates = people.map(d => new Date(`${d.birthyear}`)).sort((a, b) => a - b);
+  const sparkTicks = [new Date(Math.min(...dates)), new Date(Math.max(...dates))];
+  let count = 1;
+  const dateAndPeopleCount = dates.map(y => ({year: y, count: count++}));
 
   return (
     <header className="hero">
@@ -29,7 +34,49 @@ const Header = ({ occupation, people }) => {
         <h2 className="profile-type">Occupation</h2>
         <h1 className="profile-name">{plural(occupation.occupation)}</h1>
         <pre>
-          <img className="sparkline" src={sparklineSvg} alt={"Sparkline here"} />
+          <LinePlot
+             config={{
+               data: dateAndPeopleCount,
+               height: 100,
+               groupBy: "person",
+               legend: false,
+               shape: d => d.shape || "Line",
+               shapeConfig: {
+                 Circle: {
+                   fill: "#4B4A48",
+                   r: () => 3.5
+                 },
+                 Line: {
+                   fill: "none",
+                   stroke: "#4B4A48",
+                   strokeWidth: 1
+                 }
+               },
+               time: d => d.year,
+               timeline: false,
+               width: 275,
+               x: d => d.year,
+               xConfig: {
+                 barConfig: {"stroke-width": 0},
+                 labels: sparkTicks,
+                 shapeConfig: {
+                   fill: "#4B4A48",
+                   fontColor: "#4B4A48",
+                   fontSize: () => 8,
+                   stroke: "#4B4A48"
+                 },
+                 ticks: sparkTicks,
+                 tickSize: 0,
+                 title: "PAGE VIEWS (PV)",
+                 titleConfig: {
+                   fontColor: "#4B4A48",
+                   fontFamily: () => "Amiko",
+                   fontSize: () => 10,
+                   stroke: "#4B4A48"
+                 }
+               },
+               y: d => d.count
+             }} />
         </pre>
       </div>
     </header>
