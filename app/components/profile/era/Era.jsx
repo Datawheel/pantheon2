836acb685beedf4b024f895558272965e81ea94f
Alexander Simoes <alexandersimoes@gmail.com>
2017-03-07 16:11:12 -0500

    more viz added to era profile

--- app/components/profile/era/Era.jsx
+++ app/components/profile/era/Era.jsx
@@ -11,10 +11,9 @@ import PeopleRanking from "components/profile/era/PeopleRanking";
 import Occupations from "components/profile/era/Occupations";
 import {fetchEra, fetchEras, fetchPeopleBornInEra, fetchPeopleDiedInEra} from "actions/era";
 import {fetchAllOccupations} from "actions/occupation";
-import {Treemap} from "d3plus-react";
-import {FORMATTERS, YEAR_BUCKETS} from "types";
+import {Geomap, Priestley, StackedArea, Treemap} from "d3plus-react";
+import {COLORS_CONTINENT, FORMATTERS, YEAR_BUCKETS} from "types";
 import {bucketScale, groupBy, groupTooltip, on, peopleTooltip, shapeConfig} from "viz/helpers";
-import {extent} from "d3-array";
 
 class Era extends Component {
 
@@ -30,8 +29,11 @@ class Era extends Component {
     const yearSpan = era.end_year - era.start_year;
     const tmapBornData = peopleBornInEra
       .filter(p => p.birthyear !== null)
+      .filter(p => p.birthcountry !== null)
       .sort((a, b) => b.langs - a.langs);
     tmapBornData.forEach(d => {
+      d.borncountry = d.birthcountry.country_name;
+      d.borncontinent = d.birthcountry.continent;
       d.occupation_name = d.occupation.occupation;
       d.occupation_id = `${d.occupation_id}`;
       d.event = "CITY FOR BIRTHS OF FAMOUS PEOPLE";
@@ -62,6 +64,27 @@ class Era extends Component {
                    ? d.deathyear
                    : Math.round(d.deathyear / YEAR_BUCKETS) * YEAR_BUCKETS;
     });
+
+    const geomapData = tmapBornData.filter(d => d.place && d.place.lat_lon)
+              .concat(tmapDeathData.filter(d => d.place && d.place.lat_lon))
+              .sort((a, b) => b.langs - a.langs)
+              .slice(0, 100);
+    geomapData.forEach(d => {
+      d.place_name = d.place.name;
+      d.place_coord = d.place.lat_lon;
+      if (!(d.place_coord instanceof Array)) {
+        d.place_coord = d.place_coord
+          .replace("(", "")
+          .replace(")", "")
+          .split(",").map(Number);
+      }
+      d.place_coord.reverse();
+    });
+
+    const priestleyMax = 25;
+    const priestleyData = tmapBornData
+      .filter(p => p.deathyear !== null)
+      .slice(0, priestleyMax);
 
     const attrs = occupations.reduce((obj, d) => {
       obj[d.id] = d;
@@ -101,10 +124,116 @@ class Era extends Component {
             }} />
         ]
       },
-      {title: "Occupations Over Time", slug: "occupations_over_time", content: null},
-      {title: "Places", slug: "places", content: null},
-      {title: "Places Over Time", slug: "places_over_time", content: null},
-      {title: "Overlapping Lives", slug: "overlapping_lives", content: null}
+      {
+        title: "Occupations Over Time",
+        slug: "occupations_over_time",
+        content: null,
+        viz: [
+          <StackedArea
+            key="stacked1"
+            config={{
+              title: "Births Over Time",
+              data: tmapBornData,
+              groupBy: ["domain"].map(groupBy(attrs)),
+              shapeConfig: shapeConfig(attrs),
+              time: "logyear",
+              timeline: false,
+              tooltipConfig: groupTooltip(tmapBornData),
+              x: "logyear",
+              xConfig: {
+                tickFormat: d => FORMATTERS.year(bucketScale.invert(d))
+              },
+              y: d => d.id instanceof Array ? d.id.length : 1
+            }} />,
+          <StackedArea
+            key="stacked2"
+            config={{
+              title: "Birth Shares Over Time",
+              data: tmapBornData,
+              groupBy: ["domain"].map(groupBy(attrs)),
+              shapeConfig: shapeConfig(attrs),
+              stackOffset: "expand",
+              time: "logyear",
+              timeline: false,
+              tooltipConfig: groupTooltip(tmapBornData),
+              x: "logyear",
+              xConfig: {
+                tickFormat: d => FORMATTERS.year(bucketScale.invert(d))
+              },
+              y: d => d.id instanceof Array ? d.id.length : 1
+            }} />
+        ]
+      },
+      {
+        title: "Places",
+        slug: "places",
+        viz: [<Geomap
+                  key="geomap1"
+                  config={{
+                    title: `Major Cities in ${era.name} for Births and Deaths of Cultural Celebrities`,
+                    data: geomapData,
+                    depth: 1,
+                    groupBy: ["event", "place_name"],
+                    on: on("place", d => d.place.slug),
+                    shapeConfig: {
+                      fill: d => d.event.toLowerCase().indexOf("birth") > 0
+                               ? "rgba(76, 94, 215, 0.4)"
+                               : "rgba(95, 1, 22, 0.4)",
+                      stroke: () => "#4A4948",
+                      strokeWidth: 1,
+                      Path: {
+                        fill: "transparent",
+                        stroke: "#4A4948",
+                        strokeWidth: 0.75
+                      }
+                    },
+                    tooltipConfig: groupTooltip(geomapData, d => d.place.slug)
+                  }} />]
+      },
+      {
+        title: "Places Over Time",
+        slug: "places_over_time",
+        content: null,
+        viz: [
+          <StackedArea
+            key="stacked_country1"
+            config={{
+              title: `Birth Places of people born during the ${era.name}`,
+              data: tmapBornData,
+              depth: 1,
+              groupBy: ["borncontinent", "borncountry"],
+              on: on("place", d => d.birthcountry.slug),
+              shapeConfig: {fill: d => COLORS_CONTINENT[d.borncontinent]},
+              time: "logyear",
+              timeline: false,
+              tooltipConfig: groupTooltip(tmapBornData, d => d.birthcountry.slug),
+              x: "logyear",
+              xConfig: {
+                tickFormat: d => FORMATTERS.year(bucketScale.invert(d))
+              },
+              y: d => d.id instanceof Array ? d.id.length : 1
+            }} />
+        ]
+      },
+      {
+        title: "Overlapping Lives",
+        slug: "overlapping_lives",
+        viz: [<Priestley
+                  title={`Top ${priestleyMax} Contemporaries Born during the ${era.name}`}
+                  key="priestley1"
+                  config={{
+                    data: priestleyData,
+                    depth: 1,
+                    end: "deathyear",
+                    groupBy: ["domain", "name"].map(groupBy(attrs)),
+                    on: on("person", d => d.slug),
+                    start: "birthyear",
+                    shapeConfig: Object.assign({}, shapeConfig(attrs), {
+                      labelPadding: 2
+                    }),
+                    tooltipConfig: peopleTooltip
+                  }} />]
+      }
     ];
 
     return (
