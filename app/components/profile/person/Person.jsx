af218a3698b861f842eb9bb0885c1edf04809364
Dave Landry <landry.dave@gmail.com>
2017-04-04 17:20:14 -0400

    updates pageview/language tooltip style

--- app/components/profile/person/Person.jsx
+++ app/components/profile/person/Person.jsx
@@ -88,7 +88,7 @@ class Person extends Component {
                   body: d => {
                     let date = d.x instanceof Array ? extent(d.x) : [d.x];
                     date = date.length > 1 ? date.map(FORMATTERS.dateShort) : date.map(FORMATTERS.date);
-                    return `<span class="center">${date.join(" to ")} - ${FORMATTERS.commas(d.langs || d.views)}</span>`;
+                    return `<span class="large">${FORMATTERS.commas(d.langs || d.views)}</span><br />${date.join(" to ")}`;
                   },
                   footer: ""
                 },
