cb69f90d6eb4a0ba807b69b93b60776278de80d6
Melissa Teng <tengmelis@gmail.com>
2017-04-05 06:20:28 -0400

    footer images

--- app/components/profile/era/Footer.jsx
+++ app/components/profile/era/Footer.jsx
@@ -1,85 +1,73 @@
-import React, { Component, PropTypes } from "react";
-import styles from "css/components/profile/footer";
-import { FORMATTERS } from "types";
-import testProf from "images/test/prof.png";
+import React from "react";
+import "css/components/profile/footer";
 
-const Footer = () => {
-
-  return (
-    <footer className="profile-footer">
-      <div className="footer-container">
-        <h4 className="footer-title">Related Profiles</h4>
-        <ul className="footer-carousel-container">
-          <li className="footer-carousel-item">
-            <div className="footer-carousel-item-photo">
-              <a href="#">
-                <img src={testProf} alt={`Related profile link`} />
-              </a>
-            </div>
-            <h4 className="footer-carousel-item-title">
-              <a href="#">JK Rowling</a>
-            </h4>
-            <p>Writer</p>
-          </li>
-          <li className="footer-carousel-item">
-            <div className="footer-carousel-item-photo">
-              <a href="#">
-                <img src={testProf} alt={`Related profile link`} />
-              </a>
-            </div>
-            <h4 className="footer-carousel-item-title">
-              <a href="#">Soccer Players</a>
-            </h4>
-            <p>Occupation</p>
-          </li>
-          <li className="footer-carousel-item">
-            <div className="footer-carousel-item-photo">
-              <a href="#">
-                <img src={testProf} alt={`Related profile link`} />
-              </a>
-            </div>
-            <h4 className="footer-carousel-item-title">
-              <a href="#">Mexico</a>
-            </h4>
-            <p>Country</p>
-          </li>
-          <li className="footer-carousel-item">
-            <div className="footer-carousel-item-photo">
-              <a href="#">
-                <img src={testProf} alt={`Related profile link`} />
-              </a>
-            </div>
-            <h4 className="footer-carousel-item-title">
-              <a href="#">Rome, Italy</a>
-            </h4>
-            <p>City</p>
-          </li>
-          <li className="footer-carousel-item">
-            <div className="footer-carousel-item-photo">
-              <a href="#">
-                <img src={testProf} alt={`Related profile link`} />
-              </a>
-            </div>
-            <h4 className="footer-carousel-item-title">
-              <a href="#">Ferdinand Magellan</a>
-            </h4>
-            <p>Explorer</p>
-          </li>
-          <li className="footer-carousel-item">
-            <div className="footer-carousel-item-photo">
-              <a href="#">
-                <img src={testProf} alt={`Related profile link`} />
-              </a>
-            </div>
-            <h4 className="footer-carousel-item-title">
-              <a href="#">Frida Kahlo</a>
-            </h4>
-            <p>Painter</p>
-          </li>
-        </ul>
-      </div>
-    </footer>
-  );
-}
+const Footer = () =>
+  <footer className="profile-footer">
+    <div className="footer-container">
+      <h4 className="footer-title">Keep Exploring</h4>
+      <ul className="footer-carousel-container">
+        <li className="footer-carousel-item">
+          <div className="footer-carousel-item-photo">
+            <a href="/profile/person/isaac_newton/" className="newton">
+            </a>
+          </div>
+          <h4 className="footer-carousel-item-title">
+            <a href="/profile/person/isaac_newton/">Isaac Newton</a>
+          </h4>
+          <p>Physicist</p>
+        </li>
+        <li className="footer-carousel-item">
+          <div className="footer-carousel-item-photo">
+            <a href="/profile/person/roger_federer/" className="federer">
+            </a>
+          </div>
+          <h4 className="footer-carousel-item-title">
+            <a href="/profile/person/roger_federer/">Roger Federer</a>
+          </h4>
+          <p>Tennis Player</p>
+        </li>
+        <li className="footer-carousel-item">
+          <div className="footer-carousel-item-photo">
+            <a href="/profile/person/marie_curie/" className="curie">
+            </a>
+          </div>
+          <h4 className="footer-carousel-item-title">
+            <a href="/profile/person/marie_curie/">Marie Curie</a>
+          </h4>
+          <p>Chemist</p>
+        </li>
+        <li className="footer-carousel-item">
+          <div className="footer-carousel-item-photo">
+            <a href="/profile/person/walt_disney/" className="disney">
+            </a>
+          </div>
+          <h4 className="footer-carousel-item-title">
+            <a href="/profile/person/walt_disney/">Walt Disney</a>
+          </h4>
+          <p>Producer</p>
+        </li>
+        <li className="footer-carousel-item">
+          <div className="footer-carousel-item-photo">
+            <a href="/profile/person/laozi" className="laozi">
+            </a>
+          </div>
+          <h4 className="footer-carousel-item-title">
+            <a href="/profile/person/laozi">Laozi</a>
+          </h4>
+          <p>Philosopher</p>
+        </li>
+        <li className="footer-carousel-item">
+          <div className="footer-carousel-item-photo">
+            <a href="/profile/person/vincent_van_gogh" className="vangogh">
+            </a>
+          </div>
+          <h4 className="footer-carousel-item-title">
+            <a href="/profile/person/vincent_van_gogh">Vincent Van Gogh</a>
+          </h4>
+          <p>Painter</p>
+        </li>
+      </ul>
+    </div>
+  </footer>;
 
 export default Footer;
