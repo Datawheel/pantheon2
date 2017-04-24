af8b1e097cd209f68613f0b13a90f37dc5f37816
Melissa Teng <tengmelis@gmail.com>
2017-04-24 17:47:53 -0400

    copy edits to home site structure text

--- app/components/home/HomeIA.jsx
+++ app/components/home/HomeIA.jsx
@@ -5,41 +5,40 @@ const HomeIA = () => {
   return (
     <div className="ia">
       <h3>The Structure of Pantheon</h3>
-      <p>There are many ways to explore the Pantheon dataset:</p>
       <ul className="items ia-top">
         <li className="ia-top-bg"></li>
         <li className="item ia-top-item">
-          <h2 className="viz-explorer">Explorer</h2>
-          <p>Visually explore by building your own data visualizations and profiles.&nbsp;
-          <a href="/explore/viz" className="deep-link">Try it out</a></p>
+          <a href="/explore/viz"><h2 className="viz-explorer">Explorer</h2></a>
+          <p>Create data visualizations using the Pantheon dataset.&nbsp;
+          <a href="/explore/viz" className="deep-link">View Explorer</a></p>
         </li>
         <li className="item ia-top-item center">
           <h2 className="profiles">Profiles</h2>
         </li>
-        <li className="item ia-top-item">
-          <h2 className="ranks">Rankings</h2>
-          <p>All of Pantheon&#39;s people, places and occupations ranked by HPI (Historical Popularity Index) value.&nbsp;
-          <a href="/explore/rankings" className="deep-link">Try it out</a></p>
+        <li className="item ia-top-item right">
+          <a href="/explore/rankings"><h2 className="ranks">Rankings</h2></a>
+          <p>Rank all people, places and occupations by Historical Popularity Index (HPI).&nbsp;
+          <a href="/explore/rankings" className="deep-link">View Rankings</a></p>
         </li>
       </ul>
       <ul className="items ia-p-types">
         <li className="item ia-p-type">
-          <h2>People</h2>
+          <a href="/profile/person/"><h2>People</h2></a>
           <p>Pantheon contains data on over <span>11,000 individuals</span> whom have been classified as “historically memorable.”&nbsp;
           <a href="/profile/person/" className="deep-link">View a person</a></p>
         </li>
         <li className="item ia-p-type">
-          <h2>Places</h2>
+          <a href="/profile/place/"><h2>Places</h2></a>
           <p>We have scraped locations for every place of <span>birth</span> and <span>death</span> for every individual, present-day and across time.&nbsp;
           <a href="/profile/place/" className="deep-link">View a place</a></p>
         </li>
         <li className="item ia-p-type">
-          <h2>Occupations</h2>
+          <a href="/profile/occupation/"><h2>Occupations</h2></a>
           <p>We categorized these individuals into <span>93</span> occupations, each nested in one of <span>30</span> industries, then one of <span>9</span> domains.&nbsp;
           <a href="/profile/occupation/" className="deep-link">View an occupation</a></p>
         </li>
         <li className="item ia-p-type">
-          <h2>Eras</h2>
+          <a href="/profile/era/"><h2>Eras</h2></a>
           <p>We’ve found that spikes in number of historically memorable people coincide with eras of new <span>communication</span> technologies.&nbsp;
           <a href="/profile/era/" className="deep-link">View an era</a></p>
         </li>
