f099f1dea1783f76bb36bdf634491e91f76c36fd
Melissa Teng <tengmelis@gmail.com>
2017-05-09 18:10:00 -0400

    new sentence for data holes for Place profile
    for issue #127

--- app/components/profile/occupation/PlacesTime.jsx
+++ app/components/profile/occupation/PlacesTime.jsx
@@ -27,6 +27,7 @@ const PlacesTime = ({eras, people, occupation}) => {
         The first globally memorable {occupation.occupation} in Pantheon, <a href={`/profile/person/${oldestPerson.slug}`}>{oldestPerson.name}</a> was born in {oldestPlace ? <span><a href={`/profile/place/${oldestPlace.slug}`}>{oldestPlace.name}</a>, <a href={`/profile/place/${oldestPerson.birthcountry.slug}`}>{oldestPerson.birthcountry.name}</a></span> : null} in {FORMATTERS.year(oldestPerson.birthyear)}&nbsp;
         whereas the most recent globally memorable {occupation.occupation}, <a href={`/profile/person/${youngestPerson.slug}`}>{youngestPerson.name}</a> was born in {youngestPlace ? <span><a href={`/profile/place/${youngestPlace.slug}`}>{youngestPlace.name}</a>, <a href={`/profile/place/${youngestPerson.birthcountry.slug}`}>{youngestPerson.birthcountry.name}</a></span> : null} in {FORMATTERS.year(youngestPerson.birthyear)}.&nbsp;
         The concentration of {plural(occupation.occupation)} was largest during the <a href={`/profile/era/${eraWithMostPeople.slug}`}>{eraWithMostPeople.name}</a>, which lasted from {FORMATTERS.year(eraWithMostPeople.start_year)} to {FORMATTERS.year(eraWithMostPeople.end_year)}.
+        Some birth or death locations for earlier {plural(occupation.occupation)} are unknown, which may account for timelines differences below.
       </p>
     </div>
   );
