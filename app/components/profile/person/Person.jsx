9d8204bc58efe10f96714976a9d92bb78d7c27cd
Melissa Teng <tengmelis@gmail.com>
2017-05-09 15:24:19 -0400

    Renames person profile section “Online Attention”

--- app/components/profile/person/Person.jsx
+++ app/components/profile/person/Person.jsx
@@ -68,7 +68,7 @@ class Person extends Component {
     const sections = [
       {title: "Memorability Metrics", slug: "metrics", content: <MemMetrics pageviews={personProfile.pageviews} person={personProfile.person} />},
       {
-        title: personProfile.person.alive ? "Global Culture Career" : "Digital Afterlife",
+        title: "Online Attention",
         slug: "afterlife",
         viz: <LinePlot
               config={{
