41cb9893721033c664bb7c3726f36f9292b59924
Melissa Teng <tengmelis@gmail.com>
2017-05-12 17:19:00 -0400

    mobile Data and About pages

--- app/components/data/Data.jsx
+++ app/components/data/Data.jsx
@@ -13,22 +13,6 @@ const Data = ({children}) => {
         meta={config.meta}
         link={config.link}
       />
-      <nav className="page-nav" role="navigation">
-        <ul className="page-items">
-          <li className="item">
-            <Link to="/data/datasets" className="item-link" activeClassName="active">Download</Link>
-          </li>
-          <li className="item">
-            <Link to="/data/api" className="item-link" activeClassName="active">API</Link>
-          </li>
-          <li className="item">
-            <Link to="/data/permissions" className="item-link" activeClassName="active">Permissions</Link>
-          </li>
-          <li className="item">
-            <Link to="/data/faq" className="item-link" activeClassName="active">FAQ</Link>
-          </li>
-        </ul>
-      </nav>
       {children}
     </div>
   );
