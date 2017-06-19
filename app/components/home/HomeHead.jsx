a9125a005ab38336e74430bcb5b1240dd2748951
Alexander Simoes <alexandersimoes@gmail.com>
2017-06-19 16:36:03 -0400

    makes homepage search box more prominent closes #155

--- app/components/home/HomeHead.jsx
+++ app/components/home/HomeHead.jsx
@@ -11,13 +11,13 @@ const HomeHead = ({activateSearch}) =>
   <div className="home-head">
     <h1><img src={pantheonLogoSvg} alt="Pantheon" /></h1>
     <h3>Mapping Cultural Memory</h3>
-    <div className="home-head-content">
-      <div className="post">
-        <p><strong>Pantheon</strong> is an <a href="https://www.media.mit.edu/projects/pantheon-new/overview/" target="_blank">MIT Media Lab</a> research project celebrating the development of collective learning that endows our species with fantastic capacities. How has <HelpText text="cultural memory" msg="Defined as the accumulation of globally relevant historical characters." /> changed with the introduction of each of these <strong>Communication Technologies</strong>?</p>
-      </div>
+    <div className="home-head-content">    
       <div className="home-search">
         <img src={searchSvg} alt="Search" />
         <a onClick={activateSearch}>Search people, places, & occupations</a>
+      </div>
+      <div className="post">
+        <p><strong>Pantheon</strong> is an <a href="https://www.media.mit.edu/projects/pantheon-new/overview/" target="_blank">MIT Media Lab</a> research project celebrating the development of collective learning that endows our species with fantastic capacities. How has <HelpText text="cultural memory" msg="Defined as the accumulation of globally relevant historical characters." /> changed with the introduction of each of these <strong>Communication Technologies</strong>?</p>
       </div>
     </div>
   </div>
