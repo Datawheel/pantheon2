eaa6f0a05fe861dd36798d968d83cd3890e0ff0c
Alexander Simoes <alexandersimoes@gmail.com>
2017-06-28 14:38:21 -0400

    removes boilerplate text

--- app/components/profile/place/LivingPeople.jsx
+++ app/components/profile/place/LivingPeople.jsx
@@ -7,11 +7,6 @@ const LivingPeople = ({place, data}) => {
       <p>
         Among living memorable people from {place.name}, the three most remembered are <AnchorList items={data} name={(d) => d.name} url={(d) => `/profile/person/${d.id}/`} />. Below are the careers of the top 10 people born in {place.name}.
       </p>
-      <div className={'rank-title'}>
-        <h3>MAGICAL PLACEHOLDER: Mapping Global Career</h3>
-        <a href="#">Go to all Rankings</a>
-        <button type="button">Suggest Life Event</button>
-      </div>
     </div>
   );
 };
