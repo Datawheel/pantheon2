3ea784bf50d9f8f8f41e756ddeda38264d864bdd
Dave Landry <landry.dave@gmail.com>
2017-07-06 16:47:04 -0400

    adds progress to webpack builds

--- server/index.js
+++ server/index.js
@@ -1,20 +1,45 @@
-import express from 'express';
-import webpack from 'webpack';
-import { ENV } from './config/appConfig';
-import expressConfig from './config/express';
-import routesConfig from './config/routes';
-const App = require('../public/assets/server');
+import express from "express";
+import webpack from "webpack";
+import {ENV} from "./config/appConfig";
+import expressConfig from "./config/express";
+import routesConfig from "./config/routes";
+const App = require("../public/assets/server");
 const app = express();
 
-if (ENV === 'development') {
-  const webpackDevConfig = require('../webpack/webpack.config.dev-client');
+import ProgressPlugin from "webpack/lib/ProgressPlugin";
+
+if (ENV === "development") {
+  const webpackDevConfig = require("../webpack/webpack.config.dev-client");
   const compiler = webpack(webpackDevConfig);
-  app.use(require('webpack-dev-middleware')(compiler, {
+
+  let msgLength = 0;
+  compiler.apply(new ProgressPlugin(function(percentage, msg) {
+    const details = Array.prototype.slice.call(arguments, 2);
+    if (percentage < 1) {
+      percentage = Math.floor(percentage * 100);
+      msg = `${percentage}% ${msg}`;
+      if (percentage < 100) msg = ` ${msg}`;
+      if (percentage < 10) msg = ` ${msg}`;
+      details.forEach(detail => {
+        if (!detail) return;
+        if (detail.length > 40) detail = `...${detail.substr(detail.length - 37)}`;
+        msg += ` ${detail}`;
+      });
+      if (msg.length > msgLength) msgLength = msg.length + 1;
+      process.stdout.write(`\r${ new Array(msgLength).join(" ") }`);
+      process.stdout.write(`\r${ msg }`);
+    }
+    else {
+      process.stdout.write(`\r${ new Array(msgLength).join(" ") }\r`);
+    }
+  }));
+
+  app.use(require("webpack-dev-middleware")(compiler, {
     noInfo: true,
     publicPath: webpackDevConfig.output.publicPath
   }));
 
-  app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require("webpack-hot-middleware")(compiler));
 }
 
 /*
