5fb5990437d8030f41f5c6ecb5c98d99346be6e6
James Ferrell <hello@jamesferrell.me>
2019-06-07 15:16:02 -0400

    cleans up section heading style

--- app/pages/profile/common/SectionHead.jsx
+++ app/pages/profile/common/SectionHead.jsx
@@ -5,11 +5,11 @@ const SectionHead = ({index, numSections, title}) =>
   <div className="section-head">
     <div className="section-title">
       <h4><a id={FORMATTERS.slugify(title)}>{title}</a></h4>
-      <span className="section-nav">
+      {/*  <span className="section-nav">
         {[...Array(numSections)].map((x, i) =>
           i === index ? <span key={i} className="icons/icon-circle active"></span> : <span key={i} className="icon-circle"></span>
         )}
-      </span>
+      </span>*/}
     </div>
   </div>;
 
