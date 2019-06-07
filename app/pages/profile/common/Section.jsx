414b916084d0b06acae36ec0bd815ac3ce00384c
James Ferrell <hello@jamesferrell.me>
2019-06-07 15:16:02 -0400

    cleans up section heading style

--- app/pages/profile/common/Section.jsx
+++ app/pages/profile/common/Section.jsx
@@ -6,11 +6,11 @@ const Section = ({children, index, numSections, slug, title}) =>
     <div className="section-head">
       <div className="section-title">
         <h4><a id={slug}>{title}</a></h4>
-        <span className="section-nav">
+        {/*  <span className="section-nav">
           {[...Array(numSections)].map((x, i) =>
             i === index ? <span key={i} className="icons/icon-circle active"></span> : <span key={i} className="icon-circle"></span>
           )}
-        </span>
+        </span>*/}
       </div>
       <div className="section-actions">
         <a className="section-share"><img src="/images/icons/icon-share.svg" alt="Share this visualization" /></a>
