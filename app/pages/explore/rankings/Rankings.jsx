84e89ba8f4e7889d0c7ac3e5bd4e0c9137573c48
Alexander Simoes <alexandersimoes@gmail.com>
2019-07-22 17:52:18 -0400

    fixes bug in rankings where country dropdown was causing errors

--- app/pages/explore/rankings/Rankings.jsx
+++ app/pages/explore/rankings/Rankings.jsx
@@ -10,64 +10,13 @@ import {FORMATTERS, HPI_RANGE, LANGS_RANGE} from "types";
 import {animateScroll as scroll} from "react-scroll";
 import {plural} from "pluralize";
 import "pages/explore/Explore.css";
-
-const VizTitle = ({city, country, gender, loading, nestedOccupations, places, occupation, show, yearType}) => {
-  let occupationSubject = "";
-  let fromLocation = "";
-  let thisOcc;
-  let genderedPronoun = "people";
-
-  // Gender ---
-  if (gender === true || gender === false) {
-    genderedPronoun = gender === true ? "men" : "women";
-  }
-
-  // Occupations ---
-  if (occupation === "all") {
-    occupationSubject = genderedPronoun;
-  }
-  else if (nestedOccupations) {
-    if (occupation.includes(",")) {
-      thisOcc = nestedOccupations.map(no => no.domain).find(no => no.id === occupation).name;
-      occupationSubject = `${genderedPronoun} in ${thisOcc} occupations`;
-    }
-    else {
-      const gendersLookup = {people: "", men: "male", women: "female"};
-      thisOcc = merge(nestedOccupations.map(no => no.occupations)).find(no => `${no.id}` === occupation).occupation;
-      occupationSubject = `${gendersLookup[genderedPronoun]} ${plural(thisOcc)}`;
-    }
-  }
-
-  // Locations ---
-  if (country !== "all" && places) {
-    const countryObj = places.map(p => p.country).find(c => `${c.id}` === country);
-    if (city !== "all") {
-      const cityObj = merge(places.map(p => p.cities)).find(c => `${c.id}` === city);
-      fromLocation = ` from ${cityObj.name}, ${countryObj.country_name}`;
-    }
-    else {
-      fromLocation = ` from ${countryObj.country_name}`;
-    }
-  }
-
-  const verb = yearType === "birthyear" ? "were" : "did";
-  const predicate = yearType === "birthyear" ? "born" : "die";
-
-  const title = show === "occupations"
-    ? `What occupations were held by memorable ${occupationSubject}${fromLocation}?`
-    : `Where ${verb} memorable ${occupationSubject}${fromLocation} ${predicate}?`;
-
-  return !loading
-    ? <h1 className="explore-title">
-      {title}
-    </h1>
-    : null;
-};
+import VizTitle from "pages/explore/viz/VizTitle";
 
 class Ranking extends Component {
 
   constructor(props) {
     super(props);
+    this.countryLookup = this.props.data.places.reduce((acc, cur) => ({...acc, [cur.country.country_code]: cur.country}), {});
     this.state = {
       data: [],
       loading: true,
@@ -162,6 +111,7 @@ class Ranking extends Component {
         </div>
         <div className="explore-body">
           <Controls
+            countryLookup={this.countryLookup}
             updateData={this.updateData}
             update={this.update}
             nestedOccupations={nestedOccupations}
