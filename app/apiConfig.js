b02ae0dded2b8a57916432e149602df03860450e
Alexander Simoes <alexandersimoes@gmail.com>
2019-07-23 10:37:17 -0400

    sets default baseurl to live api

--- app/apiConfig.js
+++ app/apiConfig.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const clientConfig = {
-  host: process.env.HOSTNAME || "localhost",
-  port: process.env.PORT || "3100"
-};
+// const clientConfig = {
+//   host: process.env.HOSTNAME || "localhost",
+//   port: process.env.PORT || "3100"
+// };
+// let baseURL = `http://${clientConfig.host}:${clientConfig.port}`;
+// if (process.env.NODE_ENV === "production") {
+//   baseURL = "https://api.pantheon.world";
+// }
 
-let baseURL = `http://${clientConfig.host}:${clientConfig.port}`;
-// let baseURL = "https://api.pantheon.world";
-if (process.env.NODE_ENV === "production") {
-  baseURL = "https://api.pantheon.world";
-}
+const baseURL = "https://api.pantheon.world";
 
 export default axios.create({baseURL});
