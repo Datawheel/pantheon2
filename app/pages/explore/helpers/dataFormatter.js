e7ed967e3dc4581e1fd4a4f70666fb27fa9260d3
Alexander Simoes <alexandersimoes@gmail.com>
2019-10-16 16:44:01 -0400

    fixes rankings of places when showing death places

--- app/pages/explore/helpers/dataFormatter.js
+++ app/pages/explore/helpers/dataFormatter.js
@@ -1,7 +1,7 @@
 import {nest} from "d3-collection";
 import {mean, sum} from "d3-array";
 
-const dataFormatter = (dataArray, show) => {
+const dataFormatter = (dataArray, show, placeType) => {
   let data = dataArray;
   if (show === "occupations") {
     data = nest()
@@ -21,23 +21,44 @@ const dataFormatter = (dataArray, show) => {
       .map(d => d.value);
   }
   else if (show === "places") {
-    data = nest()
-      .key(d => d.bplace_geonameid.id)
-      .rollup(leaves => ({
-        id: leaves[0].bplace_geonameid.id,
-        name: leaves[0].bplace_geonameid.place,
-        slug: leaves[0].bplace_geonameid.slug,
-        country_name: leaves[0].bplace_country.country,
-        country_slug: leaves[0].bplace_country.slug,
-        count: leaves.length,
-        avg_hpi: mean(leaves, d => d.hpi),
-        hpi: sum(leaves, d => d.hpi),
-        avg_langs: mean(leaves, d => d.l),
-        langs: sum(leaves, d => d.l),
-        top_ranked: leaves.sort((a, b) => b.hpi - a.hpi).slice(0, 3)
-      }))
-      .entries(data.filter(d => d.bplace_geonameid && d.bplace_country))
-      .map(d => d.value);
+    if (placeType === "deathplace") {
+      data = nest()
+        .key(d => d.dplace_geonameid.id)
+        .rollup(leaves => ({
+          id: leaves[0].dplace_geonameid.id,
+          name: leaves[0].dplace_geonameid.place,
+          slug: leaves[0].dplace_geonameid.slug,
+          country_name: leaves[0].dplace_country.country,
+          country_slug: leaves[0].dplace_country.slug,
+          count: leaves.length,
+          avg_hpi: mean(leaves, d => d.hpi),
+          hpi: sum(leaves, d => d.hpi),
+          avg_langs: mean(leaves, d => d.l),
+          langs: sum(leaves, d => d.l),
+          top_ranked: leaves.sort((a, b) => b.hpi - a.hpi).slice(0, 3)
+        }))
+        .entries(data.filter(d => d.dplace_geonameid && d.dplace_country))
+        .map(d => d.value);
+    }
+    else {
+      data = nest()
+        .key(d => d.bplace_geonameid.id)
+        .rollup(leaves => ({
+          id: leaves[0].bplace_geonameid.id,
+          name: leaves[0].bplace_geonameid.place,
+          slug: leaves[0].bplace_geonameid.slug,
+          country_name: leaves[0].bplace_country.country,
+          country_slug: leaves[0].bplace_country.slug,
+          count: leaves.length,
+          avg_hpi: mean(leaves, d => d.hpi),
+          hpi: sum(leaves, d => d.hpi),
+          avg_langs: mean(leaves, d => d.l),
+          langs: sum(leaves, d => d.l),
+          top_ranked: leaves.sort((a, b) => b.hpi - a.hpi).slice(0, 3)
+        }))
+        .entries(data.filter(d => d.bplace_geonameid && d.bplace_country))
+        .map(d => d.value);
+    }
   }
   else {
     data = data.map((d, i) => Object.assign(d, {rank: i + 1}));
--- app/pages/explore/helpers/fetchPantheonData.js
+++ app/pages/explore/helpers/fetchPantheonData.js
@@ -3,6 +3,7 @@ import dataFormatter from "pages/explore/helpers/dataFormatter";
 
 const fetchPantheonData = (pageType, countryLookup, state, dataUpdateCallback) => {
   const {city, country, gender, metricCutoff, metricType, occupation, show, viz, years, yearType, placeType} = state;
+  console.log("STATEEEEE", state);
   const selectFields = "name,l,l_,age,non_en_page_views,coefficient_of_variation,hpi,id,slug,gender,birthyear,deathyear,bplace_country(id,country,continent,slug),bplace_geonameid(id,place,country,slug,lat,lon),dplace_country(id,country,slug),dplace_geonameid(id,place,country,slug),occupation_id:occupation,occupation(id,occupation,occupation_slug)";
   const apiHeaders = null;
   const sorting = "&order=hpi.desc.nullslast";
@@ -34,7 +35,7 @@ const fetchPantheonData = (pageType, countryLookup, state, dataUpdateCallback) =
   const dataUrl = `/person?select=${selectFields}&${yearType}=gte.${years[0]}&${yearType}=lte.${years[1]}${placeFilter}${occupationFilter}${genderFilter}${metricFilter}${sorting}`;
   console.log("[fetchData]: ", dataUrl);
   api.get(dataUrl, {headers: apiHeaders}).then(res => {
-    const data = pageType === "rankings" ? dataFormatter(res.data, show) : res.data;
+    const data = pageType === "rankings" ? dataFormatter(res.data, show, placeType) : res.data;
     dataUpdateCallback(Object.assign(state, {data, loading: false, show, viz}));
   });
 };
