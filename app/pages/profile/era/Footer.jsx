e89789c1e1211951079859431577419a63961e36
Alexander Simoes <alexandersimoes@gmail.com>
2019-12-05 16:04:16 -0500

    updates random person profiles and fixes broken footer links

--- app/pages/profile/era/Footer.jsx
+++ app/pages/profile/era/Footer.jsx
@@ -8,61 +8,61 @@ const Footer = () =>
       <ul className="footer-carousel-container">
         <li className="footer-carousel-item">
           <div className="footer-carousel-item-photo">
-            <a href="/profile/person/isaac_newton/" className="newton">
+            <a href="/profile/person/Isaac_Newton/" className="newton">
             </a>
           </div>
           <h4 className="footer-carousel-item-title">
-            <a href="/profile/person/isaac_newton/">Isaac Newton</a>
+            <a href="/profile/person/Isaac_Newton/">Isaac Newton</a>
           </h4>
           <p>Physicist</p>
         </li>
         <li className="footer-carousel-item">
           <div className="footer-carousel-item-photo">
-            <a href="/profile/person/roger_federer/" className="federer">
+            <a href="/profile/person/Roger_Federer/" className="federer">
             </a>
           </div>
           <h4 className="footer-carousel-item-title">
-            <a href="/profile/person/roger_federer/">Roger Federer</a>
+            <a href="/profile/person/Roger_Federer/">Roger Federer</a>
           </h4>
           <p>Tennis Player</p>
         </li>
         <li className="footer-carousel-item">
           <div className="footer-carousel-item-photo">
-            <a href="/profile/person/marie_curie/" className="curie">
+            <a href="/profile/person/Marie_Curie/" className="curie">
             </a>
           </div>
           <h4 className="footer-carousel-item-title">
-            <a href="/profile/person/marie_curie/">Marie Curie</a>
+            <a href="/profile/person/Marie_Curie/">Marie Curie</a>
           </h4>
           <p>Chemist</p>
         </li>
         <li className="footer-carousel-item">
           <div className="footer-carousel-item-photo">
-            <a href="/profile/person/walt_disney/" className="disney">
+            <a href="/profile/person/Walt_Disney/" className="disney">
             </a>
           </div>
           <h4 className="footer-carousel-item-title">
-            <a href="/profile/person/walt_disney/">Walt Disney</a>
+            <a href="/profile/person/Walt_Disney/">Walt Disney</a>
           </h4>
           <p>Producer</p>
         </li>
         <li className="footer-carousel-item">
           <div className="footer-carousel-item-photo">
-            <a href="/profile/person/laozi" className="laozi">
+            <a href="/profile/person/Laozi" className="laozi">
             </a>
           </div>
           <h4 className="footer-carousel-item-title">
-            <a href="/profile/person/laozi">Laozi</a>
+            <a href="/profile/person/Laozi">Laozi</a>
           </h4>
           <p>Philosopher</p>
         </li>
         <li className="footer-carousel-item">
           <div className="footer-carousel-item-photo">
-            <a href="/profile/person/vincent_van_gogh" className="vangogh">
+            <a href="/profile/person/Vincent_van_Gogh" className="vangogh">
             </a>
           </div>
           <h4 className="footer-carousel-item-title">
-            <a href="/profile/person/vincent_van_gogh">Vincent Van Gogh</a>
+            <a href="/profile/person/Vincent_van_Gogh">Vincent Van Gogh</a>
           </h4>
           <p>Painter</p>
         </li>
