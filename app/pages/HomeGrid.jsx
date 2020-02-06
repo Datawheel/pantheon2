caa39c1ba120fa65c9b9aef6bdabd7d559906ec1
Alexander Simoes <alexandersimoes@gmail.com>
2020-02-06 18:51:55 -0500

    adds delta of rank to homepage trending images

--- app/pages/HomeGrid.jsx
+++ app/pages/HomeGrid.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import Spinner from "components/Spinner";
 import "pages/HomeGrid.css";
 
+const formatDelta = n => {
+  if (n === 0) return "-";
+  return n > 0 ? `▲ ${Math.abs(n)}` : `▼ ${Math.abs(n)}`;
+};
+
+const deltaClassName = n => {
+  if (n === null) return "new";
+  return n >= 0 ? "up" : "down";
+};
+
 const HomeGrid = ({bios, loading, trendingLangEdition, changeTrendingLang}) =>
 
   <div className="profile-grid">
@@ -28,12 +38,16 @@ const HomeGrid = ({bios, loading, trendingLangEdition, changeTrendingLang}) =>
         {bios.sort((a, b) => a.rank - b.rank).map(profile =>
           <li className="grid-box" key={profile.pid}>
             <a href={`/profile/person/${profile.slug}`}>
+              <div className="backup-title">{profile.name}</div>
               <div className="grid-box-bg-container">
                 <div className="grid-box-bg-img-mask">
                   <div className={`grid-box-bg-img ${profile.gender}`} style={{backgroundImage: `url(/images/profile/people/${profile.pid}.jpg)`}}></div>
                 </div>
               </div>
               <h3>{profile.name}</h3>
+              {profile.rank_delta !== undefined
+                ? <div className={`rank_delta ${deltaClassName(profile.rank_delta)}`}>{profile.rank_delta !== null ? formatDelta(profile.rank_delta) : "NEW"}</div>
+                : null}
             </a>
           </li>
         )}
