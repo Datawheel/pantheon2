fca91ee3896e15a386a05ea13bf5cb78bc0cfba0
Alexander Simoes <alexandersimoes@gmail.com>
2020-02-23 22:29:08 -0500

    adds daily timeseries to pageviews to trending profiles

--- api/wikiTrends.js
+++ api/wikiTrends.js
@@ -1,5 +1,7 @@
 const createThrottle = require("async-throttle");
 const axios = require("axios");
+const nest = require("d3-collection").nest;
+const sum = require("d3-array").sum;
 
 const calcRankDeltas = (arrOfBios, day1Ago, day2Ago) => {
   const day2AgoRanks = arrOfBios
@@ -14,6 +16,106 @@ const calcRankDeltas = (arrOfBios, day1Ago, day2Ago) => {
 };
 
 module.exports = function(app) {
+
+  app.get("/api/wikiTrendDetails", async(req, res) => {
+    // wikipedia person ID
+    const wikiId = parseInt(req.query.pid, 10);
+    if (!wikiId) return res.json([]);
+
+    const dateobj = new Date();
+    // set date to yesterday
+    dateobj.setDate(dateobj.getDate() - 1);
+    const year = dateobj.getFullYear();
+    const month = `${dateobj.getMonth() + 1}`.replace(/(^|\D)(\d)(?!\d)/g, "$10$2");
+    const day = `${dateobj.getDate()}`.replace(/(^|\D)(\d)(?!\d)/g, "$10$2");
+    const yesterday = `${year}${month}${day}`;
+    dateobj.setDate(dateobj.getDate() - 29);
+    const year1monthAgo = dateobj.getFullYear();
+    const month1monthAgo = `${dateobj.getMonth() + 1}`.replace(/(^|\D)(\d)(?!\d)/g, "$10$2");
+    const day1monthAgo = `${dateobj.getDate()}`.replace(/(^|\D)(\d)(?!\d)/g, "$10$2");
+    const monthAgo = `${year1monthAgo}${month1monthAgo}${day1monthAgo}`;
+
+    let enrichedPageViewsFlat = [];
+
+    // FIRST check if this person is trending at all
+    const monthAgoTrendFromDbResp = await axios.get(`https://api.pantheon.world/trend?date=gte.${year1monthAgo}-${month1monthAgo}-${day1monthAgo}&pid=eq.${wikiId}&rank_pantheon=lte.100`).catch(e => (console.log("Pantheon DB trends read Error:", e), {data: []}));
+    // if empty it means this person is not trending
+    if (!monthAgoTrendFromDbResp.data.length) {
+      return res.json([]);
+    }
+
+    // try to get daily pageview data from db
+    const monthAgoPvFromDbResp = await axios.get(`https://api.pantheon.world/trend_pageviews?date=eq.${year1monthAgo}-${month1monthAgo}-${day1monthAgo}&pid=eq.${wikiId}`).catch(e => (console.log("Pantheon DB trends read Error:", e), {data: []}));
+    if (monthAgoPvFromDbResp.data.length) {
+      const pastMonthPvFromDbResp = await axios.get(`https://api.pantheon.world/trend_pageviews?date=gte.${year1monthAgo}-${month1monthAgo}-${day1monthAgo}&pid=eq.${wikiId}`).catch(e => (console.log("Pantheon DB trends read Error:", e), {data: []}));
+      enrichedPageViewsFlat = pastMonthPvFromDbResp.data;
+    }
+    else {
+      // Determine available language editions for this person
+      const availableLangsApi = `https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&pageids=${wikiId}&lllimit=500&llprop=langname|url&format=json&origin=*`;
+      const availableLangsResp = await axios.get(availableLangsApi).catch(e => (console.log("Wiki Trending Error:", e), {error: "Wiki ID not found"}));
+      if (availableLangsResp.error) {
+        return res.json([]);
+      }
+
+      const availableLangsJson = availableLangsResp.data;
+      if (availableLangsJson.query && availableLangsJson.query.pages) {
+        const personResult = availableLangsJson.query.pages[`${wikiId}`];
+        // return res.json(personResult);
+        if (personResult) {
+          const {langlinks} = personResult;
+          if (!langlinks) {
+            return res.json([]);
+          }
+          langlinks.unshift({"*": personResult.title, "lang": "en", "langname": "English", "url": `https://en.wikipedia.org/wiki/${personResult.title}`});
+          // const langlinksLookup = langlinks.reduce((obj, d) => (obj[d.lang] = d, obj), {});
+          const langReqs = langlinks.map(ll => `https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/${ll.lang}.wikipedia/all-access/all-agents/${encodeURIComponent(ll["*"])}/daily/${monthAgo}/${yesterday}`);
+          // return res.json({langReqs, langlinks, monthAgo, yesterday});
+
+          // throttle API queries to 20 at a time
+          const throttle = createThrottle(20);
+          const pageViews = await Promise.all(langReqs.map(url => throttle(async() => {
+            const res = await axios.get(url).catch(() => ({data: {items: []}}));
+            return res.data ? res.data.items : [];
+          })));
+
+          // filter out empty results
+          const pageViewsWithData = pageViews
+            .filter(arr => Array.isArray(arr) && arr.length);
+
+          const pageViewsFlat = [].concat.apply([], pageViewsWithData);
+
+          enrichedPageViewsFlat = pageViewsFlat.map(pv => {
+            // sample date: 2020012500
+            const formattedDate = `${pv.timestamp.slice(0, 4)}-${pv.timestamp.slice(4, 6)}-${pv.timestamp.slice(6, 8)}`;
+            return {
+              pid: wikiId,
+              date: formattedDate,
+              slug: pv.article,
+              views: pv.views,
+              lang: pv.project.replace(".wikipedia", "")
+            };
+          });
+
+          await axios.post("https://api.pantheon.world/trend_pageviews", enrichedPageViewsFlat, {
+            headers: {"Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiZGVwbG95In0.Es95xLgTB1583Sxh8MvamXIE-xEV0QsNFlRFVOq_we8"}
+          }).catch(err => (console.log(err), []));
+        }
+      }
+    }
+
+    const pvTotals = nest()
+      .key(d => d.date)
+      .entries(enrichedPageViewsFlat)
+      .map(pvData => ({
+        date: pvData.key,
+        pid: `${pvData.values[0].pid}`,
+        slug: pvData.values.find(d => d.lang === "en").slug,
+        views: sum(pvData.values, d => d.views)
+      }));
+    // pvTotals
+    return res.json(pvTotals.sort((a, b) => a.date.localeCompare(b.date)));
+  });
 
   app.get("/api/wikiTrends", async(req, res) => {
     const lang = ["ar", "zh", "nl", "en", "fr", "de", "it", "ja", "pl", "pt", "ru", "es"].indexOf(req.query.lang) !== -1 ? req.query.lang : "en";
--- app/pages/profile/person/MemMetrics.jsx
+++ app/pages/profile/person/MemMetrics.jsx
@@ -1,8 +1,40 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {FORMATTERS} from "types/index";
 import axios from "axios";
+import {FORMATTERS} from "types/index";
+import {AreaPlot} from "d3plus-react";
+import {colorLighter} from "d3plus-color";
+import {min as D3Min} from "d3-array";
+import moment from "moment";
+import {COLORS_DOMAIN} from "types";
 import "pages/profile/person/MemMetrics.css";
+
+/**
+  * returns slope, intercept and r-square of the line
+  */
+function leastSquares(xSeries, ySeries) {
+  const reduceSumFunc = (prev, cur) => prev + cur;
+
+  const xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+  const yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+
+  const ssXX = xSeries
+    .map(d => Math.pow(d - xBar, 2))
+    .reduce(reduceSumFunc);
+
+  const ssYY = ySeries
+    .map(d => Math.pow(d - yBar, 2))
+    .reduce(reduceSumFunc);
+
+  const ssXY = xSeries.map((d, i) => (d - xBar) * (ySeries[i] - yBar))
+    .reduce(reduceSumFunc);
+
+  const slope = ssXY / ssXX;
+  const intercept = yBar - xBar * slope;
+  const rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+
+  return [slope, intercept, rSquare];
+}
 
 class MemMetrics extends Component {
 
@@ -12,25 +44,35 @@ class MemMetrics extends Component {
   }
 
   componentDidMount() {
-    const {env, person} = this.props;
-
+    // const {env, person} = this.props;
     // Note: this key is restricted to Pantheon domains, if you want to use this in your
     // codebase, please generate a key: https://developers.google.com/youtube/v3/docs/
-    const apiKey = env.YOUTUBE_API_KEY;
-    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${person.name}%20${person.occupation.occupation}&maxResults=1&type=video&videoEmbeddable=true&key=${apiKey}`)
-      .then(res => {
-        const vid = res.data.items[0];
-        this.setState({vid});
-      });
+    // const apiKey = env.YOUTUBE_API_KEY;
+    // axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${person.name}%20${person.occupation.occupation}&maxResults=1&type=video&videoEmbeddable=true&key=${apiKey}`)
+    //   .then(res => {
+    //     const vid = res.data.items[0];
+    //     this.setState({vid});
+    //   });
   }
 
   render() {
-    const {pageViews, person} = this.props;
+    const {pageViews, person, wikiPageViewsPast30Days} = this.props;
+    const isTrending = wikiPageViewsPast30Days && wikiPageViewsPast30Days.length;
+    const domainColor = COLORS_DOMAIN[person.occupation.domain.toLowerCase().replace(" ", "-")];
+    // console.log(COLORS_DOMAIN, person.occupation.domain.toLowerCase());
+    const trendData = wikiPageViewsPast30Days.map((d, i) => ({...d, index: i, color: domainColor}));
     const {vid} = this.state;
     const totalPageviews = pageViews && pageViews.items ? pageViews.items
       .filter(pv => pv.views)
       .map(pv => pv.views)
       .reduce((total, newVal) => total + newVal, 0) : 0;
+
+    let [intercept, rSquare, slope, trendLineData2] = [null, null, null, []];
+    if (isTrending) {
+      [slope, intercept, rSquare] = leastSquares(trendData.map(d => d.index), trendData.map(d => d.views));
+      // console.log("slope, intercept and r-square!!!", [slope, intercept, rSquare]);
+      trendLineData2 = trendData.map((d, i) => ({date: d.date, views: Math.max(slope * i + intercept, 0), pid: "Slope", color: colorLighter(domainColor, 0.5)}));
+    }
 
     return (
       <div className="metrics-container">
@@ -47,6 +89,47 @@ class MemMetrics extends Component {
             : <button className="press-play" disabled tabIndex="-1"><i /></button>
           }
         </div> */}
+        {isTrending
+          ? <div className="metric-trending">
+            <AreaPlot config={{
+              height: 200,
+              // data: trendLineData2.concat(trendData),
+              data: trendData,
+              groupBy: "pid",
+              discrete: "x",
+              x: "date",
+              y: "views",
+              legend: false,
+              shapeConfig: {
+                Area: {
+                  curve: "monotoneX",
+                  fill: d => d.color
+                }
+              },
+              yConfig: {
+                title: "Wikipedia Pageviews"
+              },
+              time: "date",
+              title: "Pageviews for the past 30 days",
+              titleConfig: {
+                fontColor: "#4B4A48",
+                fontFamily: () => ["Amiko", "Arial Narrow", "sans-serif"]
+              },
+              tooltipConfig: {
+                footer: d => `${FORMATTERS.commas(d.views)} Page Views`,
+                title: d => `<span class="center">${moment(d.date, "YYYY-MM-DD").format("LL")}</span>`,
+                titleStyle: {"text-align": "center"},
+                width: "200px",
+                footerStyle: {
+                  "font-size": "12px",
+                  "text-transform": "none",
+                  "color": "#4B4A48"
+                }
+              }
+            }} />
+          </div>
+          : null
+        }
         <ul className="metrics-list">
           <li className="metric">
             <h4>{FORMATTERS.bigNum(totalPageviews)}</h4>
@@ -68,6 +151,12 @@ class MemMetrics extends Component {
             <h4>{FORMATTERS.decimal(person.coefficient_of_variation)}</h4>
             <p>Coefficient of Variation (CV)</p>
           </li>
+          {/* {isTrending
+            ? <li className="metric">
+              <h4>{FORMATTERS.decimal(slope)}</h4>
+              <p>30 day pageview slope</p>
+            </li>
+            : null} */}
         </ul>
       </div>
     );
