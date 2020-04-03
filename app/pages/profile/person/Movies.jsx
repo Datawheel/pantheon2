5fb64a750401a4b3645f4baeaf6d88d3a137410c
Alexander Simoes <alexandersimoes@gmail.com>
2020-04-03 16:46:13 -0400

    updates movie api endpoint to save to local db

--- app/pages/profile/person/Movies.jsx
+++ app/pages/profile/person/Movies.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {connect} from "react-redux";
 import axios from "axios";
 import moment from "moment";
 import "pages/profile/person/Works.css";
@@ -15,36 +14,11 @@ class Movies extends Component {
   }
 
   componentDidMount = () => {
-    const {env, person} = this.props;
-    const apiKey = env.TMDB_API_KEY;
-    axios.get(`https://api.themoviedb.org/3/search/person?api_key=${apiKey}&language=en-US&query=${encodeURIComponent(person.name)}&page=1&include_adult=true`)
+    const {person} = this.props;
+    axios.get(`/api/movies?id=${person.id}`)
       .then(res => {
-        // console.log("movies search res.data!!", res.data);
-        const {data: movieActorSearch} = res;
-        if (movieActorSearch.results && movieActorSearch.results.length) {
-          const movieDbPerson = movieActorSearch.results[0];
-          const {id, known_for} = movieDbPerson;
-          const knownForIds = known_for.map(d => d.id);
-          axios.get(`https://api.themoviedb.org/3/person/${id}/combined_credits?api_key=${apiKey}&language=en-US`)
-            .then(res => {
-              // console.log("credits res.data!!", res.data);
-              if (res.data) {
-                if (person.occupation.id === "FILM DIRECTOR" && res.data.crew) {
-                  const directedMovies = res.data.crew
-                    .filter(d => d.department === "Directing" && d.vote_count)
-                    .sort((a, b) => b.vote_average * b.vote_count - a.vote_average * a.vote_count)
-                    .slice(0, 6);
-                  // console.log("directedMovies!!!", directedMovies);
-                  this.setState({data: directedMovies});
-                }
-                else if (res.data.cast) {
-                  const knownForMovies = res.data.cast.filter(d => knownForIds.includes(d.id));
-                  // console.log("knownForMovies!!!", knownForMovies);
-                  this.setState({data: knownForMovies});
-                }
-              }
-            });
-        }
+        const {data} = res;
+        this.setState({data});
       });
   }
 
@@ -57,27 +31,27 @@ class Movies extends Component {
         {data.length
           ? <div className="card-list">
             {data.map(movie =>
-              <div href="#" className="work-card" key={movie.id}>
-                <div className="thumb" style={movie.poster_path ? {backgroundImage: `url('https://image.tmdb.org/t/p/original${movie.poster_path}')`} : {}}>
+              <a href={movie.homepage || `https://www.themoviedb.org/movie/${movie.mid}`} rel="noopener noreferrer" target="_blank" className="work-card" key={movie.id}>
+                <div className="thumb" style={movie.poster ? {backgroundImage: `url('${movie.poster}')`} : {}}>
                 </div>
                 <div className="info-group">
-                  <div className="title" title={movie.title || movie.original_name}>{movie.title || movie.original_name}</div>
+                  <div className="title" title={movie.title}>{movie.title}</div>
                   <div className="subtitle">{
                     person.occupation.id === "FILM DIRECTOR"
                       ? "Director"
-                      : movie.character}
+                      : movie.role}
                   </div>
                   <div className="description">{movie.overview}</div>
                   <div className="info-group-footer">
                     <span>
-                      {movie.release_date || movie.first_air_date
-                        ? moment(movie.release_date || movie.first_air_date, "YYYY-MM-DD").format("MMM Do YYYY")
+                      {movie.release_date
+                        ? moment(movie.release_date, "YYYY-MM-DD").format("MMM Do YYYY")
                         : ""}
                     </span>
-                    <span>{movie.vote_average} / 10</span>
+                    <span>{movie.average_rating} / 10</span>
                   </div>
                 </div>
-              </div>)
+              </a>)
             }
           </div>
           : null}
@@ -86,4 +60,4 @@ class Movies extends Component {
   }
 }
 
-export default connect(state => ({env: state.env}), {})(Movies);
+export default Movies;
