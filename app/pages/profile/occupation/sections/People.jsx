c123e8929e8ab1fe0e3cd3551ebef2b9e6a4ca90
Alexander Simoes <alexandersimoes@gmail.com>
2020-10-21 14:16:18 -0400

    fixes edge case in occupations with no living members

--- app/pages/profile/occupation/sections/People.jsx
+++ app/pages/profile/occupation/sections/People.jsx
@@ -22,20 +22,25 @@ const People = ({people, occupation}) => {
       <div className="section-body">
         <div>
           <p>
-          Pantheon has {FORMATTERS.commas(people.length)} people classified as {plural(occupation.occupation.toLowerCase())} born between {FORMATTERS.year(oldestBirthyear)} and {FORMATTERS.year(youngestBirthyear)}. Of these {FORMATTERS.commas(people.length)}, {FORMATTERS.commas(peopleAlive.length)} ({FORMATTERS.share(shareAlive)}) of them are still alive today.
-          The most famous living {plural(occupation.occupation.toLowerCase())} include <AnchorList items={people.filter(p => p.alive).slice(0, 3)} name={d => d.name} url={d => `/profile/person/${d.slug}/`} />.
-          {peopleDead.length
-            ? <span> The most famous deceased {plural(occupation.occupation.toLowerCase())} include <AnchorList items={people.filter(p => !p.alive).slice(0, 3)} name={d => d.name} url={d => `/profile/person/${d.slug}/`} />.</span>
-            : null }
-          {peopleNew.length
-            ? <span> As of October 2020, {peopleNew.length} new {plural(occupation.occupation.toLowerCase())} have been added to Pantheon including <AnchorList items={peopleNew.slice(0, 3)} name={d => d.name} url={d => `/profile/person/${d.slug}/`} />.</span>
-            : null }
+          Pantheon has {FORMATTERS.commas(people.length)} people classified as {plural(occupation.occupation.toLowerCase())} born between {FORMATTERS.year(oldestBirthyear)} and {FORMATTERS.year(youngestBirthyear)}. Of these {FORMATTERS.commas(people.length)}, {peopleAlive.length ? <span>{FORMATTERS.commas(peopleAlive.length)} ({FORMATTERS.share(shareAlive)})</span> : "none"} of them are still alive today.
+            {peopleAlive.length
+              ? <span> The most famous living {plural(occupation.occupation.toLowerCase())} include <AnchorList items={people.filter(p => p.alive).slice(0, 3)} name={d => d.name} url={d => `/profile/person/${d.slug}/`} />.</span>
+              : null }
+            {peopleDead.length
+              ? <span> The most famous deceased {plural(occupation.occupation.toLowerCase())} include <AnchorList items={people.filter(p => !p.alive).slice(0, 3)} name={d => d.name} url={d => `/profile/person/${d.slug}/`} />.</span>
+              : null }
+            {peopleNew.length
+              ? <span> As of October 2020, {peopleNew.length} new {plural(occupation.occupation.toLowerCase())} have been added to Pantheon including <AnchorList items={peopleNew.slice(0, 3)} name={d => d.name} url={d => `/profile/person/${d.slug}/`} />.</span>
+              : null }
           </p>
-          <div className="rank-title">
-            <h3>Living {toTitleCase(plural(occupation.occupation))}</h3>
-            <Link to={`/explore/rankings?show=people&occupation=${occupation.id}`}>Go to all Rankings</Link>
-          </div>
-          <PhotoCarousel people={peopleAlive.slice(0, 12)} rankAccessor="occupation_rank_unique" peopleAll={peopleAlive} />
+          { peopleAlive.length
+            ? <div className="rank-sec-body"><div className="rank-title">
+              <h3>Living {toTitleCase(plural(occupation.occupation))}</h3>
+              <Link to={`/explore/rankings?show=people&occupation=${occupation.id}`}>Go to all Rankings</Link>
+            </div>
+            <PhotoCarousel people={peopleAlive.slice(0, 12)} rankAccessor="occupation_rank_unique" peopleAll={peopleAlive} />
+            </div>
+            : null }
           { peopleDead.length
             ? <div className="rank-sec-body">
               <div className="rank-title">
