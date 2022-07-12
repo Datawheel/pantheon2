6eb82b2c842a22db95eae6de635ed3f2185bc930
Alexander Simoes <alexandersimoes@gmail.com>
2022-07-12 12:28:06 -0400

    updates canon to latest and removes hot reloader

--- app/pages/profile/occupation-country/OccupationCountry.jsx
+++ app/pages/profile/occupation-country/OccupationCountry.jsx
@@ -1,12 +1,11 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {hot} from "react-hot-loader/root";
-import {fetchData} from "@datawheel/canon-core";
-import {Helmet} from "react-helmet-async";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchData } from "@datawheel/canon-core";
+import { Helmet } from "react-helmet-async";
 import axios from "axios";
 import config from "helmet.js";
-import {plural} from "pluralize";
-import {toTitleCase} from "viz/helpers";
+import { plural } from "pluralize";
+import { toTitleCase } from "viz/helpers";
 import ProfileNav from "pages/profile/common/Nav";
 import Header from "pages/profile/occupation-country/Header";
 import Intro from "pages/profile/occupation-country/Intro";
@@ -18,18 +17,24 @@ import NotFound from "components/NotFound";
 import "pages/profile/common/Structure.css";
 
 class OccupationCountry extends Component {
-
   constructor(props) {
     super(props);
     this.sections = [
-      {slug: "top-10", title: "Top 10"},
-      {slug: "people", title: "People"},
-      {slug: "overlapping-lives", title: "Overlapping Lives"}
+      { slug: "top-10", title: "Top 10" },
+      { slug: "people", title: "People" },
+      { slug: "overlapping-lives", title: "Overlapping Lives" },
     ];
   }
 
   render() {
-    const {allCountriesInOccupation, allOccupationsInCountry, country, occupation, people: peopleObj, wikiSummaries} = this.props.data;
+    const {
+      allCountriesInOccupation,
+      allOccupationsInCountry,
+      country,
+      occupation,
+      people: peopleObj,
+      wikiSummaries,
+    } = this.props.data;
     const people = peopleObj.people;
 
     if (occupation === undefined || occupation.id === null) {
@@ -43,13 +48,29 @@ class OccupationCountry extends Component {
     return (
       <div>
         <Helmet
-          title={`Greatest ${country.demonym} ${toTitleCase(plural(occupation.occupation))} | Famous ${toTitleCase(plural(occupation.occupation))} from ${country.country}`}
-          meta={config.meta.map(meta => {
+          title={`Greatest ${country.demonym} ${toTitleCase(
+            plural(occupation.occupation)
+          )} | Famous ${toTitleCase(plural(occupation.occupation))} from ${
+            country.country
+          }`}
+          meta={config.meta.map((meta) => {
             if (meta.property && meta.property === "og:title") {
-              return {property: "og:title", content: `Greatest ${country.demonym} ${toTitleCase(plural(occupation.occupation))} Of All Time`};
+              return {
+                property: "og:title",
+                content: `Greatest ${country.demonym} ${toTitleCase(
+                  plural(occupation.occupation)
+                )} Of All Time`,
+              };
             }
             if (meta.property && meta.property === "og:description") {
-              return {property: "og:description", content: `A list of the best ${country.demonym} ${toTitleCase(plural(occupation.occupation))} including a ranking of the top 10 most famous ${toTitleCase(plural(occupation.occupation))} from ${country.country}.`};
+              return {
+                property: "og:description",
+                content: `A list of the best ${country.demonym} ${toTitleCase(
+                  plural(occupation.occupation)
+                )} including a ranking of the top 10 most famous ${toTitleCase(
+                  plural(occupation.occupation)
+                )} from ${country.country}.`,
+              };
             }
             return meta;
           })}
@@ -57,15 +78,28 @@ class OccupationCountry extends Component {
         <Header country={country} occupation={occupation} people={people} />
         <div className="about-section">
           <ProfileNav sections={this.sections} />
-          <Intro country={country} occupation={occupation} allCountriesInOccupation={allCountriesInOccupation} allOccupationsInCountry={allOccupationsInCountry} />
+          <Intro
+            country={country}
+            occupation={occupation}
+            allCountriesInOccupation={allCountriesInOccupation}
+            allOccupationsInCountry={allOccupationsInCountry}
+          />
         </div>
-        <TopTen country={country} occupation={occupation} people={people} wikiSummaries={wikiSummaries} />
+        <TopTen
+          country={country}
+          occupation={occupation}
+          people={people}
+          wikiSummaries={wikiSummaries}
+        />
         <People occupation={occupation} people={people} />
         <OverlappingLives people={people} occupation={occupation} />
         {/*
         <PlacesTime eras={eras} people={people} occupation={occupation} />
         <RelatedOccupations occupation={occupation} occupations={occupations} /> */}
-        <Footer allCountriesInOccupation={allCountriesInOccupation} allOccupationsInCountry={allOccupationsInCountry} />
+        <Footer
+          allCountriesInOccupation={allCountriesInOccupation}
+          allOccupationsInCountry={allOccupationsInCountry}
+        />
       </div>
     );
   }
@@ -73,35 +107,61 @@ class OccupationCountry extends Component {
 
 const occupationURL = "/occupation?occupation_slug=eq.<occupationSlug>";
 const countryURL = "/country?slug=eq.<countrySlug>";
-const allOccupationsInCountryURL = "/occupation_country?country=eq.<country.id>&order=num_people.desc.nullslast";
-const allCountriesInOccupationURL = "/occupation_country?occupation=eq.<occupation.occupation>&order=num_people.desc.nullslast";
-const peopleURL = "/person?occupation=eq.<occupation.id>&bplace_country=eq.<country.id>&order=hpi.desc.nullslast&select=bplace_geonameid(id,place,slug),bplace_country(id,continent,country,slug),dplace_country(id,continent,country,slug),dplace_geonameid(id,place,slug),occupation(id,occupation,domain,num_born,hpi,l,occupation_slug,domain_slug),occupation_id:occupation,name,slug,id,hpi,gender,birthyear,deathyear,alive,hpi_prev,l";
-const occsInDomainURL = "/occupation?domain_slug=eq.<occupation.domain_slug>&select=id";
-const wikiSummariesURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=<people.top10Ids>&origin=*";
+const allOccupationsInCountryURL =
+  "/occupation_country?country=eq.<country.id>&order=num_people.desc.nullslast";
+const allCountriesInOccupationURL =
+  "/occupation_country?occupation=eq.<occupation.occupation>&order=num_people.desc.nullslast";
+const peopleURL =
+  "/person?occupation=eq.<occupation.id>&bplace_country=eq.<country.id>&order=hpi.desc.nullslast&select=bplace_geonameid(id,place,slug),bplace_country(id,continent,country,slug),dplace_country(id,continent,country,slug),dplace_geonameid(id,place,slug),occupation(id,occupation,domain,num_born,hpi,l,occupation_slug,domain_slug),occupation_id:occupation,name,slug,id,hpi,gender,birthyear,deathyear,alive,hpi_prev,l";
+const occsInDomainURL =
+  "/occupation?domain_slug=eq.<occupation.domain_slug>&select=id";
+const wikiSummariesURL =
+  "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=<people.top10Ids>&origin=*";
 // const peopleInDomainURL = "/person?occupation=in.(<domain.occIds>)&order=l.desc&select=occupation(*),occupation_id:occupation,*";
 
 OccupationCountry.preneed = [
-  fetchData("occupation", occupationURL, res => res[0] || {id: null, domain_slug: null}),
-  fetchData("country", countryURL, res => res[0] || {id: null, domain_slug: null})
+  fetchData(
+    "occupation",
+    occupationURL,
+    (res) => res[0] || { id: null, domain_slug: null }
+  ),
+  fetchData(
+    "country",
+    countryURL,
+    (res) => res[0] || { id: null, domain_slug: null }
+  ),
 ];
 
 OccupationCountry.need = [
-  fetchData("people", peopleURL, res => {
+  fetchData("people", peopleURL, (res) => {
     const people = res;
-    const top10Ids = people.slice(0, 10).map(p => p.id).join("|");
-    return {people, top10Ids};
+    const top10Ids = people
+      .slice(0, 10)
+      .map((p) => p.id)
+      .join("|");
+    return { people, top10Ids };
   }),
-  fetchData("domain", occsInDomainURL, res => {
+  fetchData("domain", occsInDomainURL, (res) => {
     const occIds = res.reduce((arr, obj) => arr.concat([obj.id]), []);
-    return {occIds: `${occIds}`};
+    return { occIds: `${occIds}` };
   }),
-  fetchData("allOccupationsInCountry", allOccupationsInCountryURL, res => res),
-  fetchData("allCountriesInOccupation", allCountriesInOccupationURL, res => res)
+  fetchData(
+    "allOccupationsInCountry",
+    allOccupationsInCountryURL,
+    (res) => res
+  ),
+  fetchData(
+    "allCountriesInOccupation",
+    allCountriesInOccupationURL,
+    (res) => res
+  ),
 ];
 
 OccupationCountry.postneed = [
-  fetchData("wikiSummaries", wikiSummariesURL, res => res)
+  fetchData("wikiSummaries", wikiSummariesURL, (res) => res),
 ];
 
-
-export default connect(state => ({data: state.data}), {})(hot(OccupationCountry));
+export default connect(
+  (state) => ({ data: state.data }),
+  {}
+)(OccupationCountry);
--- app/pages/profile/occupation-country/SelectOccupationCountry.jsx
+++ app/pages/profile/occupation-country/SelectOccupationCountry.jsx
@@ -1,13 +1,12 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {hot} from "react-hot-loader/root";
-import {fetchData} from "@datawheel/canon-core";
-import {Helmet} from "react-helmet-async";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchData } from "@datawheel/canon-core";
+import { Helmet } from "react-helmet-async";
 import config from "helmet.js";
-import {toTitleCase} from "viz/helpers";
-import {nest} from "d3-collection";
-import {plural} from "pluralize";
-import {FORMATTERS} from "types";
+import { toTitleCase } from "viz/helpers";
+import { nest } from "d3-collection";
+import { plural } from "pluralize";
+import { FORMATTERS } from "types";
 import FancyButton from "pages/profile/common/FancyButton";
 import Select from "pages/profile/common/Select";
 import "pages/profile/common/Structure.css";
@@ -15,77 +14,98 @@ import "pages/profile/occupation-country/SelectOccupationCountry.css";
 import api from "apiConfig";
 
 class SelectOccupationCountry extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       countries: [],
       selection1: "soccer-player",
       selection2: "united-states",
-      selection2Entity: "product"
+      selection2Entity: "product",
     };
   }
 
-  setOccupation = e => {
-    const {env} = this.props;
-    const {selection2} = this.state;
-    this.setState({selection1: e.target.value});
-    api(env).get(`/occupation_country?occupation_slug=eq.${e.target.value}&order=num_people.desc.nullslast`)
-      .then(occupationCountryResults => {
+  setOccupation = (e) => {
+    const { env } = this.props;
+    const { selection2 } = this.state;
+    this.setState({ selection1: e.target.value });
+    api(env)
+      .get(
+        `/occupation_country?occupation_slug=eq.${e.target.value}&order=num_people.desc.nullslast`
+      )
+      .then((occupationCountryResults) => {
         const countries = occupationCountryResults.data;
         if (countries.length) {
-          const chosenCountryInList = countries.filter(d => d.country_slug === selection2).length;
+          const chosenCountryInList = countries.filter(
+            (d) => d.country_slug === selection2
+          ).length;
           const newSelection2 = chosenCountryInList ? selection2 : null;
-          this.setState({countries, selection2: newSelection2});
-        }
-        else {
-          this.setState({countries: []});
+          this.setState({ countries, selection2: newSelection2 });
+        } else {
+          this.setState({ countries: [] });
         }
       });
-  }
+  };
 
   render() {
-    const {selection1, selection2, selection2Entity} = this.state;
-    const {data, router} = this.props;
-    const {countries: countryList, occupations} = data;
-    const countries = this.state.countries.length ? this.state.countries : countryList;
+    const { selection1, selection2, selection2Entity } = this.state;
+    const { data, router } = this.props;
+    const { countries: countryList, occupations } = data;
+    const countries = this.state.countries.length
+      ? this.state.countries
+      : countryList;
 
     const countryIconPath = "/images/icons/country";
     const productIconPath = "/images/icons/product";
     const countryDefaultIcon = `${countryIconPath}/country.svg`;
     const productDefaultIcon = `${productIconPath}/product.svg`;
 
-    const numPeopleSum = data.occupationsInCountry.reduce((a, b) => a + (b.num_people || 0), 0);
-    let occupationsInCountry = data.occupationsInCountry.map(d => {
-      const rca = d.num_people / d.num_people_country / (d.num_people_occupation / numPeopleSum);
-      return {...d, rca};
+    const numPeopleSum = data.occupationsInCountry.reduce(
+      (a, b) => a + (b.num_people || 0),
+      0
+    );
+    let occupationsInCountry = data.occupationsInCountry.map((d) => {
+      const rca =
+        d.num_people /
+        d.num_people_country /
+        (d.num_people_occupation / numPeopleSum);
+      return { ...d, rca };
     });
     occupationsInCountry = nest()
-      .key(d => d.country_slug)
-      .rollup(leaves => leaves.sort((a, b) => b.rca - a.rca).slice(0, 5))
+      .key((d) => d.country_slug)
+      .rollup((leaves) => leaves.sort((a, b) => b.rca - a.rca).slice(0, 5))
       .entries(occupationsInCountry)
-      .map(d => {
-        const c = countryList.find(c => c.id === d.value[0].country);
-        return {values: d.value, country: c};
+      .map((d) => {
+        const c = countryList.find((c) => c.id === d.value[0].country);
+        return { values: d.value, country: c };
       });
 
     return (
       <div className="welcome">
         <Helmet
           title={"Occupation country profile selection"}
-          meta={config.meta.map(meta => {
+          meta={config.meta.map((meta) => {
             if (meta.property && meta.property === "og:title") {
-              return {property: "og:title", content: "Occupation country profile selection"};
+              return {
+                property: "og:title",
+                content: "Occupation country profile selection",
+              };
             }
             if (meta.property && meta.property === "og:description") {
-              return {property: "og:description", content: "Select a country and occupation to view a dynamic profile of the greatest people in that selection of all time."};
+              return {
+                property: "og:description",
+                content:
+                  "Select a country and occupation to view a dynamic profile of the greatest people in that selection of all time.",
+              };
             }
             return meta;
           })}
         />
         {/* welcome text */}
         <div className="welcome-intro">
-          <h1 className="welcome-intro-heading u-font-lg u-margin-top-off" aria-label="Select an occupation and country">
+          <h1
+            className="welcome-intro-heading u-font-lg u-margin-top-off"
+            aria-label="Select an occupation and country"
+          >
             Select an occupation and country
           </h1>
         </div>
@@ -94,17 +114,22 @@ class SelectOccupationCountry extends Component {
         <div className="welcome-form-outer">
           <div className="welcome-form-inner">
             <h2 className="u-visually-hidden">
-              Please select an occupation and country combination to see the most memorable biographies
+              Please select an occupation and country combination to see the
+              most memorable biographies
             </h2>
 
             {/* the form */}
-            <form onSubmit={e => this.handleSubmit(e)} className="welcome-form">
+            <form
+              onSubmit={(e) => this.handleSubmit(e)}
+              className="welcome-form"
+            >
               {/* entity 1 */}
               <div className="welcome-form-select-wrapper">
                 <img
-                  src={selection1 === "unspecified"
-                    ? countryDefaultIcon
-                    : `${countryIconPath}/country_${selection1}.png`
+                  src={
+                    selection1 === "unspecified"
+                      ? countryDefaultIcon
+                      : `${countryIconPath}/country_${selection1}.png`
                   }
                   className="welcome-form-select-icon"
                   alt=""
@@ -116,11 +141,14 @@ class SelectOccupationCountry extends Component {
                   onChange={this.setOccupation}
                 >
                   <option disabled={true}>Select an occupation</option>
-                  {occupations.map(occupation =>
-                    <option value={occupation.occupation_slug} key={occupation.occupation_slug}>
+                  {occupations.map((occupation) => (
+                    <option
+                      value={occupation.occupation_slug}
+                      key={occupation.occupation_slug}
+                    >
                       {toTitleCase(occupation.occupation)}
                     </option>
-                  )}
+                  ))}
                 </Select>
               </div>
 
@@ -128,11 +156,14 @@ class SelectOccupationCountry extends Component {
               {/* <div className="welcome-form-select-wrapper" disabled={selection1 === null}> */}
               <div className="welcome-form-select-wrapper" disabled={false}>
                 <img
-                  src={selection2Entity === "product"
-                    ? selection2 === "unspecified"
-                      ? productDefaultIcon : `${productIconPath}/hs_${selection2}.png`
-                    : selection2 === "unspecified"
-                      ? countryDefaultIcon : `${countryIconPath}/country_${selection2}.png`
+                  src={
+                    selection2Entity === "product"
+                      ? selection2 === "unspecified"
+                        ? productDefaultIcon
+                        : `${productIconPath}/hs_${selection2}.png`
+                      : selection2 === "unspecified"
+                      ? countryDefaultIcon
+                      : `${countryIconPath}/country_${selection2}.png`
                   }
                   className="welcome-form-select-icon"
                   alt=""
@@ -141,14 +172,23 @@ class SelectOccupationCountry extends Component {
                   label="country"
                   className="welcome-form-select"
                   fontSize="lg"
-                  onChange={e => this.setState({selection2: e.target.value})}
+                  onChange={(e) =>
+                    this.setState({ selection2: e.target.value })
+                  }
                 >
                   <option disabled={true}>Select a country</option>
-                  {countries.map(country =>
-                    <option value={country.country_slug || country.slug} key={country.country_slug || country.slug}>
-                      {country.num_people ? `${country.country} (${FORMATTERS.commas(country.num_people)})` : country.country}
+                  {countries.map((country) => (
+                    <option
+                      value={country.country_slug || country.slug}
+                      key={country.country_slug || country.slug}
+                    >
+                      {country.num_people
+                        ? `${country.country} (${FORMATTERS.commas(
+                            country.num_people
+                          )})`
+                        : country.country}
                     </option>
-                  )}
+                  ))}
                 </Select>
               </div>
 
@@ -157,38 +197,54 @@ class SelectOccupationCountry extends Component {
                 <FancyButton
                   icon="arrow-right"
                   disabled={selection1 === "unspecified"}
-                  onClick={() => router.push(`/profile/occupation/${selection1}/country/${selection2}`)}>
+                  onClick={() =>
+                    router.push(
+                      `/profile/occupation/${selection1}/country/${selection2}`
+                    )
+                  }
+                >
                   Go to profile
                 </FancyButton>
               </div>
-
             </form>
           </div>
         </div>
 
         {/* welcome text */}
         <div className="sample-ctr-occs">
-          <h2 className="welcome-intro-heading u-font-lg u-margin-top-off" aria-label="The most famous occupation / country combinations">
+          <h2
+            className="welcome-intro-heading u-font-lg u-margin-top-off"
+            aria-label="The most famous occupation / country combinations"
+          >
             Who are the most famous...
           </h2>
           <ul className="sample-ctr-occs-list">
-            {occupationsInCountry.map(aCountry =>
+            {occupationsInCountry.map((aCountry) => (
               <li key={aCountry.country.slug}>
                 <ul className="sample-ctr-occs">
-                  {aCountry.values.map(occupationInCountry =>
-                    <li key={`${occupationInCountry.country_slug}-${occupationInCountry.occupation_slug}`}>
-                      <a href={`/profile/occupation/${occupationInCountry.occupation_slug}/country/${occupationInCountry.country_slug}`}>
-                        <img className="country-flag" src={`/images/icons/country/${aCountry.country.slug}.svg`} />
-                        <span>{aCountry.country.demonym} {toTitleCase(plural(occupationInCountry.occupation))}</span>
+                  {aCountry.values.map((occupationInCountry) => (
+                    <li
+                      key={`${occupationInCountry.country_slug}-${occupationInCountry.occupation_slug}`}
+                    >
+                      <a
+                        href={`/profile/occupation/${occupationInCountry.occupation_slug}/country/${occupationInCountry.country_slug}`}
+                      >
+                        <img
+                          className="country-flag"
+                          src={`/images/icons/country/${aCountry.country.slug}.svg`}
+                        />
+                        <span>
+                          {aCountry.country.demonym}{" "}
+                          {toTitleCase(plural(occupationInCountry.occupation))}
+                        </span>
                       </a>
                     </li>
-                  )}
+                  ))}
                 </ul>
               </li>
-            )}
+            ))}
           </ul>
         </div>
-
       </div>
     );
   }
@@ -201,7 +257,10 @@ const countriesURL = "/country?order=num_born.desc.nullslast";
 SelectOccupationCountry.preneed = [
   fetchData("occupations", occupationsURL),
   fetchData("countries", countriesURL),
-  fetchData("occupationsInCountry", allOccupationsInCountryURL)
+  fetchData("occupationsInCountry", allOccupationsInCountryURL),
 ];
 
-export default connect(state => ({data: state.data, env: state.env}), {})(hot(SelectOccupationCountry));
+export default connect(
+  (state) => ({ data: state.data, env: state.env }),
+  {}
+)(SelectOccupationCountry);
