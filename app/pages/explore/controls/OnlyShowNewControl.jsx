dcdb65df87c53eeae909f73f399da27318df0f9c
Alexander Simoes <alexandersimoes@gmail.com>
2022-07-12 13:03:30 -0400

    replaces all 2020 references to 2022

--- app/pages/explore/controls/OnlyShowNewControl.jsx
+++ app/pages/explore/controls/OnlyShowNewControl.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import {connect} from "react-redux";
-import {updateOnlyShowNew} from "actions/vb";
+import { connect } from "react-redux";
+import { updateOnlyShowNew } from "actions/vb";
 
-const OnlyShowNewControl = ({loading, onlyShowNew, updateOnlyShowNew}) =>
+const OnlyShowNewControl = ({ loading, onlyShowNew, updateOnlyShowNew }) => (
   <div className="flat-options">
     <label
       disabled={loading}
       className={onlyShowNew ? "active" : ""}
-      htmlFor="onlyNew">Only new biographies (2020)</label>
-    <input disabled={loading} type="checkbox" id="onlyNew" name="scales" onChange={e => updateOnlyShowNew(e.target.checked)} checked={onlyShowNew} />
+      htmlFor="onlyNew"
+    >
+      Only new biographies (2022)
+    </label>
+    <input
+      disabled={loading}
+      type="checkbox"
+      id="onlyNew"
+      name="scales"
+      onChange={(e) => updateOnlyShowNew(e.target.checked)}
+      checked={onlyShowNew}
+    />
   </div>
-  ;
+);
+const mapDispatchToProps = { updateOnlyShowNew };
 
-const mapDispatchToProps = {updateOnlyShowNew};
-
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   loading: state.vb.data.loading,
-  onlyShowNew: state.vb.onlyShowNew
+  onlyShowNew: state.vb.onlyShowNew,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(OnlyShowNewControl);
