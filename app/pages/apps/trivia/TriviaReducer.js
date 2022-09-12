f6a25de7edd15773b9f5d2516d8d30617dbb70e2
Mariana Macedo <marianagmmacedo@gmail.com>
2022-09-12 16:45:31 +0200

    Updating trivia game to use the database and update the games from CSV

--- app/pages/apps/trivia/TriviaReducer.js
+++ app/pages/apps/trivia/TriviaReducer.js
@@ -4,12 +4,41 @@ import {
   SET_CURRENT_ANSWER,
   SET_ERROR,
   SET_SHOW_RESULTS,
-  RESET_QUIZ
+  RESET_QUIZ,
+  UPDATE_DATE,
+  GET_GAME_ID,
+  GET_QUESTION_ID,
+  UPDATE_CONSENT,
+  SAVE_CONSENT,
 } from "pages/apps/trivia/reducerTypes.js";
+
+function convertTZ(date, tzString) {
+  return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+}
 
 /** */
 function TriviaReducer(state, action) {
   switch (action.type) {
+    case GET_GAME_ID:
+      return {
+        ...state,
+        gameId: action.gameId
+      };
+    case UPDATE_CONSENT:
+      return {
+        ...state,
+        isOpen: action.isOpen
+      };
+    case SAVE_CONSENT:
+      return {
+        ...state,
+        saveConsent: action.saveConsent
+      };
+    case GET_QUESTION_ID:
+      return {
+        ...state,
+        questionId: action.questionId
+      };
     case SET_CURRENT_ANSWER:
       return {
         ...state,
@@ -35,13 +64,32 @@ function TriviaReducer(state, action) {
         ...state,
         answers: action.answers
       };
+    case UPDATE_DATE:
+      return {
+        ...state,
+        currentDate: convertTZ(new Date(), "Europe/Paris")
+      };
     case RESET_QUIZ:
+      const date = convertTZ(new Date(), "Europe/Paris");
+      const day = date.getDate();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+      const month = date.getMonth();
       return {
         ...state,
         answers: [],
         currentQuestion: 0,
         currentAnswer: "",
         showResults: false,
+        timeLeft: {
+          days: day,
+          month: month,
+          hours: hours,
+          minutes: minutes,
+          seconds: seconds,
+          spentTime : 0
+        },
         error: "",
         questions: action.questions
       };
