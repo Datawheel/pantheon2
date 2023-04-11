5c292317b6e945fffea9dc8f82496374c5c85b6a
Alexander Simoes <alexandersimoes@gmail.com>
2023-04-11 14:23:38 -0400

    rewrite majority of sections for person page

--- components/utils/AnchorList.jsx
+++ components/utils/AnchorList.jsx
@@ -1,30 +1,46 @@
-import { Tooltip } from "@blueprintjs/core";
+// import { Tooltip } from "@blueprintjs/core";
 import Link from "next/link";
 
-const AnchorList = ({ items, name, tooltip, url, noAnd, newWindow }) => (
-  <span>
-    {items.map((item, index) => (
-      <span key={item.id || index}>
-        {!noAnd && index && index === items.length - 1 ? " and " : null}
-        {tooltip ? (
-          <Tooltip content={tooltip(item)}>
-            <a
-              href={url(item)}
-              target={newWindow ? "_blank" : "_self"}
-              rel={newWindow ? "noopener" : null}
-            >
-              {name(item)}
-            </a>
-          </Tooltip>
-        ) : (
+export default function AnchorList({
+  items,
+  name,
+  tooltip,
+  url,
+  noAnd,
+  newWindow,
+}) {
+  return (
+    <span>
+      {items.map((item, index) => (
+        <span key={item.id || index}>
+          {!noAnd && index && index === items.length - 1 ? " and " : null}
           <Link href={url(item)} target={newWindow ? "_blank" : "_self"}>
             {name(item)}
           </Link>
-        )}
-        {items.length !== 2 ? (index < items.length - 1 ? ", " : null) : null}
-      </span>
-    ))}
-  </span>
-);
-
-export default AnchorList;
+          {items.length !== 2 ? (index < items.length - 1 ? ", " : null) : null}
+        </span>
+      ))}
+    </span>
+  );
+  // <span>
+  //   {items.map((item, index) => (
+  //     <span key={item.id || index}>
+  //       {!noAnd && index && index === items.length - 1 ? " and " : null}
+  //       {tooltip ? (
+  //         <a
+  //           href={url(item)}
+  //           target={newWindow ? "_blank" : "_self"}
+  //           rel={newWindow ? "noopener" : null}
+  //         >
+  //           {name(item)}
+  //         </a>
+  //       ) : (
+  //         <Link href={url(item)} target={newWindow ? "_blank" : "_self"}>
+  //           {name(item)}
+  //         </Link>
+  //       )}
+  //       {items.length !== 2 ? (index < items.length - 1 ? ", " : null) : null}
+  //     </span>
+  //   ))}
+  // </span>;
+}
