26eb03d7b0bd198a599990a12b19b7f39dbae4b2
Alexander Simoes <alexandersimoes@gmail.com>
2023-05-01 18:09:26 -0400

    occupation in country bilateral profile

---
+++ app/profile/occupation/[id]/country/[countryId]/page.jsx
@@ -0,0 +1,113 @@
+import Header from "/components/occupation-country/Header";
+import Intro from "/components/occupation-country/Intro";
+import TopTen from "/components/occupation-country/Sections/TopTen";
+import People from "/components/occupation-country/Sections/People";
+import Lifespans from "/components/occupation-country/Sections/Lifespans";
+import Footer from "/components/occupation-country/Sections/Footer";
+
+async function getOccupations() {
+  const res = await fetch(
+    "https://api.pantheon.world/occupation?order=num_born.desc.nullslast&select=id,occupation,domain,num_born,hpi,l,occupation_slug,domain_slug"
+  );
+  return res.json();
+}
+
+async function getOccupation(occupationId) {
+  const res = await fetch(
+    `https://api.pantheon.world/occupation?occupation_slug=eq.${occupationId}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.pgrst.object+json",
+      },
+    }
+  );
+  return res.json();
+}
+
+async function getCountry(countryId) {
+  const res = await fetch(
+    `https://api.pantheon.world/country?slug=eq.${countryId}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.pgrst.object+json",
+      },
+    }
+  );
+  return res.json();
+}
+
+async function getAllCountriesInOccupation(occupationId) {
+  const res = await fetch(
+    `https://api.pantheon.world/occupation_country?occupation=eq.${occupationId}&order=num_people.desc.nullslast`
+  );
+  return res.json();
+}
+
+async function getAllOccupationsInCountry(countryId) {
+  const res = await fetch(
+    `https://api.pantheon.world/occupation_country?country=eq.${countryId}&order=num_people.desc.nullslast`
+  );
+  return res.json();
+}
+
+async function getPeople(occupationId, countryId) {
+  const res = await fetch(
+    `https://api.pantheon.world/person?occupation=eq.${occupationId}&bplace_country=eq.${countryId}&order=hpi.desc.nullslast&select=bplace_geonameid(id,place,slug),bplace_country(id,continent,country,slug),dplace_country(id,continent,country,slug),dplace_geonameid(id,place,slug),occupation(id,occupation,domain,num_born,hpi,l,occupation_slug,domain_slug),occupation_id:occupation,name,slug,id,hpi,gender,birthyear,deathyear,alive,hpi_prev,l`
+  );
+  return res.json();
+}
+
+export default async function Page({ params: { id, countryId } }) {
+  const [occupations, occupation, country] = await Promise.all([
+    getOccupations(),
+    getOccupation(id),
+    getCountry(countryId),
+  ]);
+
+  const [allCountriesInOccupation, allOccupationsInCountry, people] =
+    await Promise.all([
+      getAllCountriesInOccupation(occupation.occupation),
+      getAllOccupationsInCountry(country.id),
+      getPeople(occupation.id, country.id),
+    ]);
+
+  const attrs = occupations.reduce((obj, d) => {
+    obj[d.id] = d;
+    return obj;
+  }, {});
+
+  return (
+    <div className="person">
+      <Header country={country} occupation={occupation} people={people} />
+      <div className="about-section">
+        {/* <ProfileNav sections={this.sections} /> */}
+        <Intro
+          country={country}
+          occupation={occupation}
+          allCountriesInOccupation={allCountriesInOccupation}
+        />
+      </div>
+      <TopTen country={country} occupation={occupation} people={people} />
+      <People
+        country={country}
+        occupation={occupation}
+        people={people}
+        title={"People"}
+        slug={"people"}
+      />
+      <Lifespans
+        attrs={attrs}
+        people={people}
+        occupation={occupation}
+        slug={"overlapping-lives"}
+        title={"Overlapping Lives"}
+      />
+      <Footer
+        allCountriesInOccupation={allCountriesInOccupation}
+        allOccupationsInCountry={allOccupationsInCountry}
+      />
+    </div>
+  );
+}
--- components/common/Select.jsx
+++ components/common/Select.jsx
@@ -1,73 +1,67 @@
-import React, { Component } from "react";
-import { Icon } from "@blueprintjs/core";
+// import { Icon } from "@blueprintjs/core";
 // import "./Select.css";
 
-export default class Select extends Component {
-  render() {
-    const {
-      className,
-      disabled,
-      namespace, // "cp" (default) or "cms"
-      fontSize,
-      inline,
-      label, // the label
-      labelHidden, // hide the label (label still required)
-      options, // array of options
-      children, // when creating options from an array isn't enough
-      value, // select value
-      tabIndex,
-      onChange, // callback function; select a new option
-    } = this.props;
+export default function Select({
+  className,
+  disabled,
+  namespace, // "cp" (default) or "cms"
+  fontSize,
+  inline,
+  label, // the label
+  labelHidden, // hide the label (label still required)
+  options, // array of options
+  children, // when creating options from an array isn't enough
+  value, // select value
+  tabIndex,
+  onChange, // callback function; select a new option
+}) {
+  // remove stringified nulls
+  const filteredOptions = options && options.filter((option) => option !== "");
 
-    // remove stringified nulls
-    const filteredOptions =
-      options && options.filter((option) => option !== "");
-
-    return (options && options.length) || children ? (
-      <label
-        className={`${
-          className ? `${className} ` : ""
-        }${namespace}-select-label u-font-${fontSize}${
-          inline ? " cms-inline-select-label" : ""
-        }`}
-      >
-        {label && (
-          <span
-            className={`${namespace}-select-text${
-              label && labelHidden ? " u-visually-hidden" : ""
-            }`}
-          >
-            {label}
-          </span>
-        )}
-
-        <Icon className={`${namespace}-select-icon`} icon="caret-down" />
-
-        <select
-          disabled={disabled}
-          className={`${namespace}-select ${
-            disabled ? `${namespace}-select-disabled` : ""
+  return (options && options.length) || children ? (
+    <label
+      className={`${
+        className ? `${className} ` : ""
+      }${namespace}-select-label u-font-${fontSize}${
+        inline ? " cms-inline-select-label" : ""
+      }`}
+    >
+      {label && (
+        <span
+          className={`${namespace}-select-text${
+            label && labelHidden ? " u-visually-hidden" : ""
           }`}
-          onChange={onChange}
-          value={value}
-          tabIndex={tabIndex}
         >
-          {options &&
-            filteredOptions.map((option) => (
-              <option value={option} key={`select-option-${option}`}>
-                {option}
-              </option>
-            ))}
-          {children}
-        </select>
-      </label>
-    ) : (
-      "missing or malformed `options` or `children` prop in Select.jsx"
-    );
-  }
+          {label}
+        </span>
+      )}
+
+      {/* <Icon className={`${namespace}-select-icon`} icon="caret-down" /> */}
+
+      <select
+        disabled={disabled}
+        className={`${namespace}-select ${
+          disabled ? `${namespace}-select-disabled` : ""
+        }`}
+        onChange={onChange}
+        value={value}
+        tabIndex={tabIndex}
+      >
+        {options &&
+          filteredOptions.map((option) => (
+            <option value={option} key={`select-option-${option}`}>
+              {option}
+            </option>
+          ))}
+        {children}
+      </select>
+    </label>
+  ) : (
+    "missing or malformed `options` or `children` prop in Select.jsx"
+  );
 }
 
-Select.defaultProps = {
-  fontSize: "sm",
-  namespace: "cp",
-};
+// Select.defaultProps = {
+//   fontSize: "sm",
+//   namespace: "cp",
+// };
