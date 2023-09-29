371561edad1413ff38eb7da99263eaf76db71066
Alexander Simoes <alexandersimoes@gmail.com>
2023-09-29 10:23:10 -0400

    adds missing files

---
+++ components/explore/OnlyShowNewControl.jsx
@@ -0,0 +1,29 @@
+"use client";
+import { useDispatch, useSelector } from "react-redux";
+import { updateOnlyShowNew } from "../../features/exploreSlice";
+
+export default function MetricCutoffControl() {
+  const loading = false;
+  const dispatch = useDispatch();
+  const { onlyShowNew } = useSelector((state) => state.explore);
+
+  return (
+    <div className="flat-options">
+      <label
+        disabled={loading}
+        className={onlyShowNew ? "active" : ""}
+        htmlFor="onlyNew"
+      >
+        Only new biographies (2022)
+      </label>
+      <input
+        disabled={loading}
+        type="checkbox"
+        id="onlyNew"
+        name="scales"
+        onChange={(e) => dispatch(updateOnlyShowNew(e.target.checked))}
+        checked={onlyShowNew}
+      />
+    </div>
+  );
+}
