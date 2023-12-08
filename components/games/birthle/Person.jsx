0cefe1ed5d5fd0e793937755f0f1f93d305a4322
Alexander Simoes <alexandersimoes@gmail.com>
2023-12-08 17:30:46 -0500

    adds birthle person endpoint

---
+++ components/games/birthle/Person.jsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function Person({ data, onClick, isBoardItem, dataKey }) {
+  const [imgURL, setImgURL] = useState(data.imgURL);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = data.imgURL;
+
+    img.onerror = () => {
+      setImgURL("https://pantheon.world/images/icons/icon-person.svg");
+    };
+  });
+
+  return (
+    <li
+      className={`${isBoardItem ? "board-row-list-item" : "panel-list-item"}`}
+      key={dataKey ? `${dataKey}-${data.id}` : data.id.toString()}
+      onClick={onClick}
+    >
+      <div
+        className={`card ${isBoardItem ? "board-item" : ""}`}
+        id={data.id}
+        key={dataKey ? `${dataKey}-${data.id}` : data.id.toString()}
+      >
+        <img
+          className="card-image"
+          src={imgURL}
+          key={`Photo${data.id}`}
+          alt={`Photo of ${data.name}`}
+        />
+        {!isBoardItem ? (
+          <div key={`cardTitle_${data.id}`} className="card-title">
+            {data.name}
+          </div>
+        ) : null}
+      </div>
+    </li>
+  );
+}
