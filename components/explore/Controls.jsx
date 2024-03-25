9fb74b95a61701515aa8355a7c145deaf044d2d9
Alexander Simoes <alexandersimoes@gmail.com>
2024-03-25 14:31:13 -0400

    adds rankings table

---
+++ components/explore/Controls.jsx
@@ -0,0 +1,55 @@
+"use client";
+import {useSelector} from "react-redux";
+import VizControl from "./VizControl";
+import ShowControl from "./ShowControl";
+import GenderControl from "./GenderControl";
+import YearControl from "./YearControl";
+import PlaceControl from "./PlaceControl";
+import OccupationControl from "./OccupationControl";
+import MetricCutoffControl from "./MetricCutoffControl";
+import OnlyShowNewControl from "./OnlyShowNewControl";
+
+const toggleSidePanel = () => {
+  console.log("toggle side panel!");
+};
+
+export default function Controls({places, nestedOccupations}) {
+  const {page, show} = useSelector(state => state.explore);
+
+  return (
+    <div className="explore-controls viz-explorer" id="side-panel">
+      {/* desktop title*/}
+      <div className="control-header desktop">
+        <h2 className="viz-explorer">Visualizations</h2>
+      </div>
+      {/* mobile toggle */}
+      <button className="control-header mobile" onClick={toggleSidePanel}>
+        <h2 className="viz-explorer">
+          <span className="helper-text">Open </span>
+          {page === "rankings" ? "Rankings" : "Visualizations"}
+          <span className="helper-text"> Panel</span>
+        </h2>
+        <i className="control-icon" />
+      </button>
+
+      <section className="control-group main-selector">
+        {page === "viz" ? <VizControl /> : null}
+        <ShowControl />
+      </section>
+
+      <section className="control-group">
+        <GenderControl />
+        {/* <YearControl years={years} changeYears={this.updateAndFetchData} yearType={yearType} /> */}
+        <YearControl />
+        {show.type !== "places" ? <PlaceControl places={places} /> : null}
+        <OccupationControl nestedOccupations={nestedOccupations} />
+      </section>
+
+      <section className="control-group advanced-group">
+        <h3>Advanced Options</h3>
+        <MetricCutoffControl />
+        <OnlyShowNewControl />
+      </section>
+    </div>
+  );
+}
---
+++ components/explore/ShowControl.jsx
@@ -0,0 +1,188 @@
+"use client";
+import {useDispatch, useSelector} from "react-redux";
+import {updateShowDepth, updateShowType} from "../../features/exploreSlice";
+
+const rankingTypes = [
+  {id: "people", name: "People"},
+  {id: "places", name: "Places"},
+  {id: "occupations", name: "Occupations"},
+];
+
+export default function ShowControl() {
+  const loading = false;
+  const dispatch = useDispatch();
+  const {page, show} = useSelector(state => state.explore);
+  let pageRankingTypes = rankingTypes;
+
+  if (page === "viz") {
+    pageRankingTypes = pageRankingTypes.slice(1, pageRankingTypes.length);
+  }
+
+  const {type: showType, depth: showDepth} = show;
+
+  return (
+    <div className="filter">
+      <h3>Group People by</h3>
+      <ul className="items filter options viztype-options">
+        {pageRankingTypes.map(rt => (
+          <li key={rt.id} value={rt.id}>
+            <h4>
+              <a
+                href="#"
+                data-id={rt.id}
+                onClick={e =>
+                  loading
+                    ? e.preventDefault()
+                    : (e.preventDefault(),
+                      dispatch(
+                        updateShowType({showType: rt.id.toLowerCase(), page})
+                      ))
+                }
+                className={`${rt.id} ${loading ? "disabled" : null} ${
+                  !showType || showType === rt.id ? "active" : ""
+                }`}
+              >
+                {rt.name}
+              </a>
+            </h4>
+          </li>
+        ))}
+      </ul>
+      {showType === "occupations" && page === "rankings" ? (
+        <div className="options filter">
+          <h3>Data Depth</h3>
+          <ul className="items options viztype-options">
+            <li>
+              <h4>
+                <a
+                  href="#"
+                  id="occupations"
+                  onClick={e =>
+                    loading
+                      ? e.preventDefault()
+                      : (e.preventDefault(),
+                        dispatch(
+                          updateShowDepth({
+                            showType,
+                            showDepth: "occupations",
+                            page,
+                          })
+                        ))
+                  }
+                  className={`d-3 ${
+                    !showDepth || showDepth === "occupations" ? "active" : ""
+                  }`}
+                >
+                  Occupation
+                </a>
+              </h4>
+            </li>
+            <li>
+              <h4>
+                <a
+                  href="#"
+                  id="industries"
+                  onClick={e =>
+                    loading
+                      ? e.preventDefault()
+                      : (e.preventDefault(),
+                        dispatch(
+                          updateShowDepth({
+                            showType,
+                            showDepth: "industries",
+                            page,
+                          })
+                        ))
+                  }
+                  className={`d-2 ${
+                    showDepth === "industries" ? "active" : ""
+                  }`}
+                >
+                  Industry
+                </a>
+              </h4>
+            </li>
+            <li>
+              <h4>
+                <a
+                  href="#"
+                  id="domains"
+                  onClick={e =>
+                    loading
+                      ? e.preventDefault()
+                      : (e.preventDefault(),
+                        dispatch(
+                          updateShowDepth({
+                            showType,
+                            showDepth: "domains",
+                            page,
+                          })
+                        ))
+                  }
+                  className={`d-1 ${showDepth === "domains" ? "active" : ""}`}
+                >
+                  Domain
+                </a>
+              </h4>
+            </li>
+          </ul>
+        </div>
+      ) : null}
+      {showType === "places" && page === "rankings" ? (
+        <div className="options filter">
+          <h3>Data Depth</h3>
+          <ul className="items options viztype-options">
+            <li>
+              <h4>
+                <a
+                  href="#"
+                  id="places"
+                  onClick={e =>
+                    loading
+                      ? e.preventDefault()
+                      : (e.preventDefault(),
+                        dispatch(
+                          updateShowDepth({
+                            showType,
+                            showDepth: "places",
+                            page,
+                          })
+                        ))
+                  }
+                  className={`d-2 ${
+                    !showDepth || showDepth === "places" ? "active" : ""
+                  }`}
+                >
+                  City
+                </a>
+              </h4>
+            </li>
+            <li>
+              <h4>
+                <a
+                  href="#"
+                  id="countries"
+                  onClick={e =>
+                    loading
+                      ? e.preventDefault()
+                      : (e.preventDefault(),
+                        dispatch(
+                          updateShowDepth({
+                            showType,
+                            showDepth: "countries",
+                            page,
+                          })
+                        ))
+                  }
+                  className={`d-1 ${showDepth === "countries" ? "active" : ""}`}
+                >
+                  Country
+                </a>
+              </h4>
+            </li>
+          </ul>
+        </div>
+      ) : null}
+    </div>
+  );
+}
