5d581adcac70c3565b4601ddc708f65d62087b55
Alexander Simoes <alexandersimoes@gmail.com>
2024-03-27 12:50:35 -0400

    adds missing libs

---
+++ locales/server.js
@@ -0,0 +1,6 @@
+// locales/server.js
+import { createI18nServer } from "next-international/server";
+
+export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
+  en: () => import("./en"),
+});
