f9ef18aa9b4bd5cd3d8e306c5000916121f6d900
Alexander Simoes <alexandersimoes@gmail.com>
2024-04-03 16:39:18 -0400

    fixes broken links in footer

---
+++ app/[locale]/about/layout.jsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+
+import "/styles/Misc.css";
+import "/styles/About.css";
+
+export default function AboutLayout({children}) {
+  return (
+    <div className="about-page">
+      <nav className="page-nav" role="navigation">
+        <ul className="page-items">
+          <li className="item">
+            <Link
+              href="/about/vision"
+              className="item-link"
+              activeClassName="is-active"
+            >
+              Vision
+            </Link>
+          </li>
+          <li className="item">
+            <Link
+              href="/about/methods"
+              className="item-link"
+              activeClassName="is-active"
+            >
+              Methods
+            </Link>
+          </li>
+          <li className="item">
+            <Link
+              href="/about/team"
+              className="item-link"
+              activeClassName="is-active"
+            >
+              Team
+            </Link>
+          </li>
+          <li className="item">
+            <Link
+              href="/about/publications"
+              className="item-link"
+              activeClassName="is-active"
+            >
+              Publications
+            </Link>
+          </li>
+          <li className="item">
+            <Link
+              href="/about/data_sources"
+              className="item-link"
+              activeClassName="is-active"
+            >
+              Data Sources
+            </Link>
+          </li>
+          <li className="item">
+            <Link
+              href="/about/contact"
+              className="item-link"
+              activeClassName="is-active"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </div>
+  );
+}
---
+++ app/[locale]/about/privacy/page.jsx
@@ -0,0 +1,19 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>Pantheon Privacy Policy</h1>
+      <p>
+        Due to the fact that Pantheon includes YouTube video embeds, which is
+        owned by Google, we are inluding a link to the{" "}
+        <a
+          href="https://policies.google.com/privacy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Google Privacy Policy
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
