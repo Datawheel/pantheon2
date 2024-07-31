7fcb84075d07ab90a2d293bdba88d3cfe081b5e3
nspmx <nico.perezmundaca@gmail.com>
2024-07-31 11:26:19 -0400

    initial docker commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // This will build the project as a standalone app inside the Docker image
+  output: 'standalone',
+};
 
 export default nextConfig;
