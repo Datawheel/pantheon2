145c1a8125e44e8ea526687b8b1c8a42ca69a28e
Alexander Simoes <alexandersimoes@gmail.com>
2025-01-03 14:39:42 -0500

    reduces default revalidation time to 1 day

--- app/constants.js
+++ app/constants.js
@@ -1,6 +1,6 @@
 // Universal time periods
 export const REVALIDATE_PERIODS = {
-  DEFAULT: 7 * 24 * 60 * 60, // 7 days in seconds
+  DEFAULT: 1 * 24 * 60 * 60, // 1 day in seconds
   SHORT: 60 * 60, // 1 hour in seconds
   LONG: 30 * 24 * 60 * 60, // 30 days in seconds
   // Add more as needed
