c853ff5ba910ebe5bb3e987ae56fa61f4f3642d5
Alexander Simoes <alexandersimoes@gmail.com>
2025-01-17 13:49:37 -0500

    ensures use of api domain variable

--- components/person/YearRanking.jsx
+++ components/person/YearRanking.jsx
@@ -8,6 +8,7 @@ import {
 import AnchorList from "../utils/AnchorList";
 import PhotoCarousel from "../utils/PhotoCarousel";
 import SectionLayout from "../common/SectionLayout";
+import {BASE_API} from "/app/constants";
 
 async function getBirthYearRankings(
   birthYear,
@@ -15,7 +16,7 @@ async function getBirthYearRankings(
   birthYearRankHigh
 ) {
   const res = await fetch(
-    `https://api.pantheon.world/person_ranks?birthyear=eq.${birthYear}&birthyear_rank_unique=gte.${birthYearRankLow}&birthyear_rank_unique=lte.${birthYearRankHigh}&order=birthyear_rank_unique&select=occupation,bplace_country,hpi,birthyear_rank,birthyear_rank_unique,slug,gender,name,id,birthyear,deathyear`
+    `${BASE_API}/person_ranks?birthyear=eq.${birthYear}&birthyear_rank_unique=gte.${birthYearRankLow}&birthyear_rank_unique=lte.${birthYearRankHigh}&order=birthyear_rank_unique&select=occupation,bplace_country,hpi,birthyear_rank,birthyear_rank_unique,slug,gender,name,id,birthyear,deathyear`
   );
   return res.json();
 }
@@ -26,7 +27,7 @@ async function getDeathYearRankings(
   deathYearRankHigh
 ) {
   const res = await fetch(
-    `https://api.pantheon.world/person_ranks?deathyear=eq.${deathyear}&deathyear_rank_unique=gte.${deathYearRankLow}&deathyear_rank_unique=lte.${deathYearRankHigh}&order=deathyear_rank_unique&select=occupation,dplace_country,hpi,deathyear_rank,deathyear_rank_unique,slug,gender,name,id,deathyear,birthyear`
+    `${BASE_API}/person_ranks?deathyear=eq.${deathyear}&deathyear_rank_unique=gte.${deathYearRankLow}&deathyear_rank_unique=lte.${deathYearRankHigh}&order=deathyear_rank_unique&select=occupation,dplace_country,hpi,deathyear_rank,deathyear_rank_unique,slug,gender,name,id,deathyear,birthyear`
   );
   return res.json();
 }
