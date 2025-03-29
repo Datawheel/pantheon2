043afa228ff50814a2e475248fd5b51efe95937c
Alexander Simoes <alexandersimoes@gmail.com>
2025-03-29 18:36:57 -0300

    removes search_domain_filter since it is now a tier 3 feature

--- app/api/whyTrending/route.js
+++ app/api/whyTrending/route.js
@@ -87,7 +87,7 @@ export async function GET(request) {
         messages,
         temperature: 0.2,
         top_p: 0.9,
-        search_domain_filter: ["perplexity.ai"],
+        // search_domain_filter: ["perplexity.ai"],
         return_images: false,
         return_related_questions: false,
         search_recency_filter: "month",
